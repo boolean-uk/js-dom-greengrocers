92422973fa55ee45d78576b5a7d28aa8bf46e95c
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-22 15:58:01 +0200

    added event to addToCartBtn

--- src/index.js
+++ src/index.js
@@ -58,17 +58,22 @@ const storeItemList = document.querySelector(".store--item-list");
 
 state.items.forEach((veg) => {
   const vegLi = document.createElement("li");
+
   const vegImgWrapper = document.createElement("div");
   vegImgWrapper.setAttribute("class", "store--item-icon");
+
   const vegImg = document.createElement("img");
   vegImg.src = `assets/icons/${veg.id}.svg`;
   vegImg.alt = veg.name;
+
   const addToCartBtn = document.createElement("button");
   addToCartBtn.innerText = "Add To Cart";
 
   vegImgWrapper.append(vegImg);
-
   vegLi.append(vegImgWrapper, addToCartBtn);
-
   storeItemList.append(vegLi);
+
+  addToCartBtn.addEventListener("click", () => {
+    state.cart.push({ ...veg, quantity: 1 });
+  });
 });
