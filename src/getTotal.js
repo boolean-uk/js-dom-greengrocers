938bf52f8d15e9ee5db67d6ebabbbb1031240876
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-25 18:25:30 +0200

    changed uncohesive variable names

--- src/getTotal.js
+++ src/getTotal.js
@@ -2,12 +2,12 @@ import state from "./state.js";
 
 const getTotal = () => {
   const totalNumber = document.querySelector(".total-number");
-  const priceArr = state.cart.map((veg) => {
-    return veg.price * veg.quantity;
+  const priceArr = state.cart.map((item) => {
+    return item.price * item.quantity;
   });
 
-  const totalPrice = priceArr.reduce((veg1, veg2) => {
-    return veg1 + veg2;
+  const totalPrice = priceArr.reduce((item1, item2) => {
+    return item1 + item2;
   }, 0);
 
   totalNumber.innerText = `£${Number(
