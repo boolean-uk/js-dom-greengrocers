ed221d2cabadc0befa52b42d1ada902569f2fe80
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-25 18:35:36 +0200

    removed unnecessary return

--- src/getTotal.js
+++ src/getTotal.js
@@ -2,13 +2,8 @@ import state from "./state.js";
 
 const getTotal = () => {
   const totalNumber = document.querySelector(".total-number");
-  const priceArr = state.cart.map((item) => {
-    return item.price * item.quantity;
-  });
-
-  const totalPrice = priceArr.reduce((item1, item2) => {
-    return item1 + item2;
-  }, 0);
+  const priceArr = state.cart.map((item) => item.price * item.quantity);
+  const totalPrice = priceArr.reduce((item1, item2) => item1 + item2, 0);
 
   totalNumber.innerText = `£${Number(
     Number.parseFloat(totalPrice).toFixed(3)
