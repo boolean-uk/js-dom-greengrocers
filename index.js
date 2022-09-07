2cb8ce1e94ebc72ae74c06abf858d1fb9e036802
Alexander Lind <alex.anonym@gmail.com>
2022-09-07 12:28:05 +0200

    finished both extensions

--- index.js
+++ index.js
@@ -4,51 +4,65 @@ const state = {
       id: "001-beetroot",
       name: "beetroot",
       price: 0.35,
+      type: 'vegetable',
     },
     {
       id: "002-carrot",
       name: "carrot",
       price: 0.35,
+      type: 'vegetable',
+
     },
     {
       id: "003-apple",
       name: "apple",
       price: 0.35,
+      type: 'fruit',
     },
     {
       id: "004-apricot",
       name: "apricot",
       price: 0.35,
+      type: 'fruit',
+
     },
     {
       id: "005-avocado",
       name: "avocado",
       price: 0.35,
+      type: 'vegetable',
     },
     {
       id: "006-bananas",
       name: "bananas",
       price: 0.35,
+      type: 'fruit',
+
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
       price: 0.35,
+      type: 'vegetable',
     },
     {
       id: "008-berry",
       name: "berry",
       price: 0.35,
+      type: 'fruit',
     },
     {
       id: "009-blueberry",
       name: "blueberry",
       price: 0.35,
+      type: 'fruit',
     },
     {
       id: "010-eggplant",
       name: "eggplant",
       price: 0.35,
+      type: 'vegetable',
+      type: 'fruit',
     },
   ],
   cart: [],
@@ -147,6 +161,114 @@ const updateTotalPrice = () => {
   totalPrice.innerHTML = "£" + totalPriceText.toFixed(2);
   return totalPriceText;
 };
+
+const filterByVegetables = () => {
+console.log('Filtered by vegetables')
+
+const itemList = document.querySelector(".item-list");
+itemList.innerHTML = "";
+
+state.items.forEach((item) => {
+
+  if (item.type === "vegetable") {
+  const storeItemList = document.createElement("li"); // List item is created
+  itemList.append(storeItemList);
+  const storeItemIcon = document.createElement("div"); // Icon is created
+  storeItemIcon.className = "store--item-icon";
+  storeItemIcon.innerHTML = `<img src="assets/icons/${item.id}.svg" alt="beetroot" />`;
+  storeItemList.append(storeItemIcon);
+
+  const storeItemButton = document.createElement("button"); // Button is created
+  storeItemButton.innerHTML = "Add to cart";
+  storeItemButton.addEventListener("click", (event) => {
+    // Button event
+    event.preventDefault();
+    if (state.cart.includes(item)) {
+      alert("Add to cart failed, only one item of the same type can be added");
+      return ;
+    }
+    state.cart.push(item);
+    state.cart[state.cart.indexOf(item)].itemQuantity = 1;
+    renderCartItemList(item);
+    updateTotalPrice();
+  });
+  storeItemList.append(storeItemButton);
+}});
+}
+
+const filterByFruits = () => {
+
+const itemList = document.querySelector(".item-list");
+itemList.innerHTML = "";
+
+state.items.forEach((item) => {
+
+  if (item.type === "fruit") {
+  const storeItemList = document.createElement("li"); // List item is created
+  itemList.append(storeItemList);
+  const storeItemIcon = document.createElement("div"); // Icon is created
+  storeItemIcon.className = "store--item-icon";
+  storeItemIcon.innerHTML = `<img src="assets/icons/${item.id}.svg" alt="beetroot" />`;
+  storeItemList.append(storeItemIcon);
+
+  const storeItemButton = document.createElement("button"); // Button is created
+  storeItemButton.innerHTML = "Add to cart";
+  storeItemButton.addEventListener("click", (event) => {
+    // Button event
+    event.preventDefault();
+    if (state.cart.includes(item)) {
+      alert("Add to cart failed, only one item of the same type can be added");
+      return ;
+    }
+    state.cart.push(item);
+    state.cart[state.cart.indexOf(item)].itemQuantity = 1;
+    renderCartItemList(item);
+    updateTotalPrice();
+  });
+  storeItemList.append(storeItemButton);
+}});
+
+}
+
+const sortAlphabetically = () => {
+  const sortArray = (x,y) => {
+    return x.name.localeCompare(y.name);
+  }
+  console.log('sortAlphabetically');
+  const itemsSortedAlphabetically = state.items.sort(sortArray);
+  console.log(itemsSortedAlphabetically);
+  
+  const itemList = document.querySelector(".item-list");
+  itemList.innerHTML = "";
+
+  itemsSortedAlphabetically.forEach((item) => {
+    const storeItemList = document.createElement("li"); // List item is created
+    itemList.append(storeItemList);
+    const storeItemIcon = document.createElement("div"); // Icon is created
+    storeItemIcon.className = "store--item-icon";
+    storeItemIcon.innerHTML = `<img src="assets/icons/${item.id}.svg" alt="beetroot" />`;
+    storeItemList.append(storeItemIcon);
+
+    const storeItemButton = document.createElement("button"); // Button is created
+    storeItemButton.innerHTML = "Add to cart";
+    storeItemButton.addEventListener("click", (event) => {
+      // Button event
+      event.preventDefault();
+      if (state.cart.includes(item)) {
+        alert("Add to cart failed, only one item of the same type can be added");
+        return ;
+      }
+      state.cart.push(item);
+      state.cart[state.cart.indexOf(item)].itemQuantity = 1;
+      renderCartItemList(item);
+      updateTotalPrice();
+    });
+    storeItemList.append(storeItemButton);
+  });
+
+}
+
+
 
 const init = () => {
   renderStoreItemList();
