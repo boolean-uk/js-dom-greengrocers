f586abf62263ef9fb9aee32523fa3642cd281d5f
James Byrne <james.c.byrne1994@gmail.com>
2022-09-07 17:07:43 +0100

    added groucery, still cart to do

--- index.js
+++ index.js
@@ -53,3 +53,58 @@ const state = {
   ],
   cart: []
 };
+
+//create DOM selectors
+const greenGrocers = document.querySelector('.store--item-list' )
+const userCart = document.querySelector('.cart--item-list')
+
+const itemArray = state.items
+//console.log('test array ',itemArray )
+
+//const fruitAndVegName = state.items[i].name
+//console.log('test fruit ', fruitAndVegName)
+
+
+const createLi = () => {
+  for(let i = 0; i < itemArray.length; i++){
+    const myLi = document.createElement('li')
+    const theDiv = document.createElement('div')
+    const foodButton = document.createElement("button");
+
+
+    foodButton.textContent = ' add to cart'
+    console.log(foodButton)
+    theDiv.classList = "store--item-icon"
+    // const foodDiv = document.createElement('div')
+    greenGrocers.append(myLi)
+    myLi.append(theDiv)
+
+    const fruitAndVegId = state.items[i].id
+    const imageOfFood = document.createElement('img')
+    //console.log(fruitAndVegId)
+     imageOfFood.setAttribute('src', "/assets/icons/" + fruitAndVegId + ".svg" )
+    // console.log(" the image here is ", imageOfFood)
+    theDiv.append(imageOfFood)
+    myLi.append(foodButton)
+  }
+
+}
+
+createLi()
+
+
+
+
+
+
+// function renderCart() {
+//   state.cart.forEach( item => {
+//     //create cart elements for each item and appent them to the cart ul
+//   })  
+//  }
+
+//  addToCartBtn.addEventListener('click', function(){
+//    userCart.push(item)
+//    renderCart()
+//  })
+
