2e3462461c5368652bff4b7b464a0ced89ab16c2
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2022-09-08 09:29:55 +0100

    in progress

--- index.js
+++ index.js
@@ -55,24 +55,118 @@ const state = {
 };
 
 let groceryList = document.querySelector('.store--item-list')
+const cartItemList = document.querySelector('.cart--item-list')
+// console.log('in global scope it is', cartItemList)
+// const userCart = state.cart
+
 
 
 function createStoreItemList() {
 for (let i=0; i < state.items.length; i++) {
     const item = state.items[i]
-    console.log(i, item);
 
     const listItem = document.createElement('li')
-      listItem.innerText = item.name
-      groceryList.append(listItem)
+    listItem.innerText = item.name
+    groceryList.append(listItem)
 
-      // adding an image to listItem//
+      // adding image to listItem//
       const itemImage = document.createElement('div')
       itemImage.setAttribute('class', 'store--item-icon')
-      itemImage.innerHTML = `<img src="assets/icons/${item.id}.svg" alt="beetroot" />`
+      itemImage.innerHTML = `<img src="assets/icons/${item.id}.svg" alt="${item.name}" />`
       listItem.append(itemImage)
 
+      // adding button to listItem//
+      const addToCartBtn = document.createElement('button')
+      addToCartBtn.innerHTML = 'ADD TO CART'
+      listItem.append(addToCartBtn)
+
+      //renderCart function//
+      function renderCart() {
+        state.cart.forEach( item => {
+        cartItem = document.createElement('li')
+        cartItemList.append(cartItem)
+
+        cartItemImg = document.createElement('img')
+        cartItemImg.setAttribute('src', `assets/icons/${item.id}.svg`)
+        cartItemImg.setAttribute('class', 'cart--item-icon')
+        cartItemImg.setAttribute('alt', `${item.name}`)
+        cartItem.append(cartItemImg)
+         let sum = 1
+
+
+      //adding <p> to cartItem//
+        const pEl = document.createElement('p')
+        pEl.innerText = item.name
+
+      //creating 1st button within cartItem//
+        const buttonRemove = document.createElement('button')
+        buttonRemove.innerText = '-'
+        buttonRemove.setAttribute('class', 'quantity-btn remove-btn center')
+
+      //creating span within cartItem //
+        const spanEl = document.createElement('span')
+        spanEl.setAttribute('class', 'quantity-text center')
+        spanEl.innerHTML = item.quantity;
+
+
+      //creating 2nd button within cartItem//
+          const buttonAdd = document.createElement('button')
+          buttonAdd.setAttribute('class', 'quantity-btn add-btn center')
+          buttonAdd.innerText = '+'
+
+      //adding all to cartItem //
+      cartItem.append(pEl, buttonRemove, spanEl, buttonAdd)
+
+      // calculating total sum//
+      function totalCart() {
+        const total = document.querySelector(".total-number")
+        total.innerHTML = ""
+        let sumTotal
+      
+        let updatedCart = state.cart.map((a) => a.price)
+      
+        if (updatedCart.length <= 0) {
+          total.textContent = "£0.00"
+          return;
+        }
+      
+        sumTotal = updatedCart.reduce((pv, cv) => pv + cv)
+      
+        total.textContent = "£" + Math.round(sumTotal * 100) / 100
+      
+        console.log(`updatedcart is`, updatedCart)
+      }
+
+      //adding event listener to remove button - unfinished//
+
+        buttonRemove.addEventListener("click", () => {
+          if (spanEl.textContent <= 1) {
+            cartItem.remove();
+        }
+
+        let removeIndex = state.cart.indexOf(item);
+        state.cart.splice(removeIndex, 1)
+        sum--;
+        spanEl.textContent = sum
+        console.log(`sum is`, sum)
+        totalCart()
+        })
+
+
+      })
+
     }
+
+      //adding event listener to add button - unfinished//
+      addToCartBtn.addEventListener("click", () => {
+      state.cart.push(item)
+      console.log(item)
+
+      renderCart()
+      })
+
+      
+  }
 }
 createStoreItemList()
 
