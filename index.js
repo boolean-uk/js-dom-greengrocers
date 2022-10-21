9e850b591e0e181695d916b8693fee7eca6ddc61
Dave <Daverevell491@gmail.com>
2022-10-21 15:58:46 +0100

    Render store images working

--- index.js
+++ index.js
@@ -54,20 +54,42 @@ const state = {
   cart: []
 };
 
+const storeItemList = document.querySelector('.store--item-list')
+
 // Create function for store items
+function renderStore(items) {
   // Clear store-item--list
+  storeItemList.innerHTML = ''
 
   // Create an li for each object in state.items
-  // Create a div to contain the img with class 'store--item-icon'
-  // Implement food item img/svg into the div
+  items.forEach(item => {
+    const li = document.createElement('li')
+    // Create a div to contain the img with class 'store--item-icon'
+    const div = document.createElement('div')
+    div.setAttribute('class', 'store--item-icon')
+    // Implement food item img/svg into the div
+    const img = document.createElement('img')
+    img.setAttribute('src', `assets/icons/${item.id}.svg`)
+    img.setAttribute('alt', item.name)
+    
+
+    //Append everything to where it should be
+    storeItemList.appendChild(li)
+    li.appendChild(div)
+    div.appendChild(img)
+  })
+  
+  
+  
 
   // Implement button into the li
   // Event Listener for when the button is clicked
     //pushes clicked item/object into state.cart
-    
+
 // function end
+}
 
-
+function renderCart() {
 // Create function for cart
   // Clear cart--item-list-container
 
@@ -88,11 +110,13 @@ const state = {
     // Add one to the displayed amount
 
 // function end
+}
 
-
+function renderTotal() {
 // Create function for displaying total
 
   // Create sum of the cart item price * cart item quantity
   // Append sum into span with class total-number
 
 //function end
+}
