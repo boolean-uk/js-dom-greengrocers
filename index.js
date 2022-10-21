0bfc12c55f90594728027b8c9fbb584052bbb715
Dave <Daverevell491@gmail.com>
2022-10-21 13:44:18 +0100

    Comments added#

--- index.js
+++ index.js
@@ -53,3 +53,46 @@ const state = {
   ],
   cart: []
 };
+
+// Create function for store items
+  // Clear store-item--list
+
+  // Create an li for each object in state.items
+  // Create a div to contain the img with class 'store--item-icon'
+  // Implement food item img/svg into the div
+
+  // Implement button into the li
+  // Event Listener for when the button is clicked
+    //pushes clicked item/object into state.cart
+    
+// function end
+
+
+// Create function for cart
+  // Clear cart--item-list-container
+
+  // Create an li for each item/object in state.cart
+  // Add the item/object img within the li
+  // Create a p element within each li containing state.cart.name
+
+  // Create minus button within the lis with class 'quantity-btn remove-btn center'
+  
+  // Create amount number display within lis with class 'quantity-text center'
+  
+  // Create plus button within lis with class 'quantity-btn add-btn center'
+
+  // Event listener for when minus button is clicked
+    // Minus one from the displayed amount
+
+  // Event listener for when plus button is clicked
+    // Add one to the displayed amount
+
+// function end
+
+
+// Create function for displaying total
+
+  // Create sum of the cart item price * cart item quantity
+  // Append sum into span with class total-number
+
+//function end
