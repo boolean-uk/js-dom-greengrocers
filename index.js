8aa21f509d048b6df24579b0bcc8ecc8b43fce3e
= <yemunRussom10@gmail.com>
2022-10-22 20:53:33 +0100

    Greengrocers section is done

--- index.js
+++ index.js
@@ -53,3 +53,65 @@ const state = {
   ],
   cart: []
 };
+const groceries = state.items;
+
+const gorcersList = document.querySelector('.store--item-list');
+const itemCartList = document.querySelector('.cart--item-list')
+
+// Rendering items function
+
+function renderItems(){
+  groceries.forEach(item => {
+    const srcImg = 'assets/icons/' + item.id + '.svg';  
+    const gorcersItem = document.createElement('li');
+    const div = document.createElement('div');
+    const gorceryItemImage = document.createElement('img');
+    gorceryItemImage.setAttribute('src', srcImg);
+    const addToCart = document.createElement('button'); 
+    addToCart.innerText = "Add to cart";
+    
+    div.appendChild(gorceryItemImage);
+    gorcersItem.appendChild(div);
+    gorcersItem.appendChild(addToCart);
+    gorcersList.appendChild(gorcersItem);
+
+    addToCart.addEventListener('click', () =>{
+            showSelectedItem(item);
+    })
+    
+    gorceryItemImage.addEventListener('click', () =>{
+      showSelectedItem(item)
+    })
+    
+  }); 
+}
+
+renderItems();
+
+function showSelectedItem(item){
+  const itemCart = document.createElement('li');
+  const itemCartImg = document.createElement('img');
+  itemCartImg.setAttribute('src', 'assets/icons/' + item.id + '.svg')
+  const itemCartName = document.createElement('p');
+  itemCartName.innerText = item.name;
+  const reduceItem = document.createElement('button');
+  reduceItem.innerText = "-";
+  const quntityCartItem = document.createElement('span');
+  quntityCartItem.innerText = 1;
+  const increaseCartItem = document.createElement('button');
+  increaseCartItem.innerText = "+";  
+  itemCart.appendChild(itemCartImg);
+  itemCart.appendChild(itemCartName);
+  itemCart.appendChild(reduceItem);
+  itemCart.appendChild(quntityCartItem);
+  itemCart.appendChild(increaseCartItem);
+  itemCartList.appendChild(itemCart);
+}
+
+// js
+// first create function to render the image of the item to the header putting it in div as defined in the store.html
+// insert button called "add cart" with the image in div, it is clickable, by setting the addEventlistner.
+// to add the image to the cart and calculate the total price based on the number of added items.
+// Once added to the cart, - and + button should be added along side the image of the item.
+// to decrease and increase the number of items in the cart and calculate the amount and price of
+// the total added items. 
