9d9a9a38068a66bab9275b584236c5243cbb00db
Enky <enkh6at@yahoo.com>
2022-10-23 20:34:21 +0100

    added some more jsDOM

--- index.js
+++ index.js
@@ -57,15 +57,55 @@ const shop = document.querySelector(".store--item-list");
 const cart = document.querySelector(".cart--item-list");
 state.items.forEach(item => {
   console.log("eachOne", item)
+  const li = document.createElement("li")
+  const div = document.createElement("div")
+  div.setAttribute("class", "store--item-icon")
+  const img = document.createElement("img")
+  img.src = `assets/icons/${item.id}.svg`
+  img.alt = `${item.name}`
+  const addButton = document.createElement("button")
+  addButton.innerText="add to cart"
+  addButton.addEventListener("click", function (e){
+    e.preventDefault();
+    state.cart.push(item)
+    console.log("what is that", state)
+  })
+  li.append(div, addButton)
+  div.append(img)
+  shop.append(li)
 })
+console.log("after method", state.items[4].name)
 // const items = state.items
 // console.log("shop is:", items[1].id)
-function fromShop (shopItem){
-  const li = document.createElement("li")
-  console.log("branch", li)
-  const div = document.createElement("div")
-  const img = `src="assets/icons/001-beetroot.svg"`
+// function fromShop (shopItem){
+  // const li = document.createElement("li")
+  // console.log("branch", li)
+  // const div = document.createElement("div")
+  // const img = `src="assets/icons/001-beetroot.svg"`
 
-  return li
+//   return li
+// }
+// fromShop()
+function cartItem () {
+  state.items.forEach(item => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.setAttribute("class", "cart--item-icon")
+    img.src = `assets/icons/${item.id}.svg`;
+    img.alt = `${item.name}`;
+    const p = document.createElement("p");
+    p.innerText = `${item.name}`;
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "quantity-btn remove-btn center");
+    removeButton.innerText = "-";
+    const span = document.createElement("span")
+    span.innerText = "";
+    const addButton = document.createElement("button")
+    addButton.setAttribute("class", "quantity-btn add-btn center")
+    addButton.innerText = "+";
+    cart.append(li);
+    li.append(img, p, removeButton, span, addButton)
+    return li
+  })
 }
-fromShop()
+cartItem()
