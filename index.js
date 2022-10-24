5770ad9a656748dfb5920f078d91bba97807318d
OlhaDorofiychuk <olha.d@yahoo.com>
2022-10-24 19:48:51 +0100

    done with filter extension

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const state = {
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35,
+      price: 0.55,
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35,
+      price: 0.37,
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35,
+      price: 0.75,
     },
     {
       id: "005-avocado",
@@ -28,27 +28,27 @@ const state = {
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35,
+      price: 0.25,
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35,
+      price: 0.56,
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35,
+      price: 0.15,
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35,
+      price: 0.95,
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35,
+      price: 0.41,
     },
   ],
   cart: [],
@@ -60,11 +60,16 @@ const vegetables = [
   "007-bell-pepper",
   "010-eggplant",
 ];
+//CONSTANT
+const type_vegetables = "vegetables";
+const type_fruits = "fruits";
 const storeItems = document.querySelector(".store--item-list");
 const cartItems = document.querySelector(".cart--item-list");
 const addToCartButton = document.querySelector(".add");
 const h1 = document.querySelector("h1");
+const span = document.querySelector(".quantity");
 
+//FILTERS
 const allFilter = document.createElement("button");
 allFilter.innerText = "All";
 h1.appendChild(allFilter);
@@ -94,68 +99,136 @@ function addStoreItem(item) {
   addToCartButton.addEventListener("click", () => {
     addToCart(item);
   });
-
-  allFilter.addEventListener("click", () => {
-    li.remove();
-    addStoreItem(item);
-  });
-
-  vegetablesFilter.addEventListener("click", () => {
-    li.remove();
-  });
-  fruitsFilter.addEventListener("click", () => {
-    li.remove();
-  });
 }
 
-function render() {
-  state.items.forEach((item) => {
+allFilter.addEventListener("click", () => {
+  render(state.items);
+});
+
+vegetablesFilter.addEventListener("click", () => {
+  const vegetablesFilterArray = state.items.filter((item) => {
+    return item.type === "vegetables";
+  });
+  render(vegetablesFilterArray);
+});
+
+fruitsFilter.addEventListener("click", () => {
+  const fruitsFilterArray = state.items.filter((item) => {
+    return item.type === "fruits";
+  });
+  render(fruitsFilterArray);
+});
+
+function render(renderItems) {
+  // let renderArray = [];
+  // if (renderItems.length > 0) {
+  //   renderArray = renderItems;
+  // } else {
+  //   renderArray = state.items;
+  // }
+  storeItems.innerHTML = "";
+  renderItems.forEach((item) => {
     item.quantity = 1;
     item.image = `assets/icons/${item.id}.svg`;
+    if (vegetables.includes(item.id)) {
+      item.type = "vegetables";
+    } else {
+      item.type = "fruits";
+    }
+
     addStoreItem(item);
   });
 }
-
 function addToCart(item) {
   if (state.cart.includes(item)) {
+    console.log(state.cart);
+    item.quantity = item.quantity += 1;
+    const li = document.querySelector("#" + `cart-item-${item.id}`);
+    const span = li.querySelector(".quantity");
+    span.innerText = item.quantity;
+    calculateTotal();
     return;
+  } else {
+    item.quantity = 1;
+    const li = document.createElement("li");
+    li.setAttribute("id", `cart-item-${item.id}`);
+    const img = document.createElement("img");
+    img.setAttribute("class", "cart--item-icon");
+    img.setAttribute("src", item.image);
+    img.setAttribute("alt", item.name);
+    const p = document.createElement("p");
+    p.innerText = item.name;
+    const minusButton = document.createElement("button");
+    minusButton.setAttribute("class", "remove-btn");
+    minusButton.innerText = "-";
+
+    const span = document.createElement("span");
+    span.setAttribute("class", "quantity");
+    span.innerText = item.quantity;
+    const plusButton = document.createElement("button");
+    plusButton.setAttribute("class", "add-btn");
+    plusButton.innerText = "+";
+    li.appendChild(img);
+    li.appendChild(p);
+    li.appendChild(minusButton);
+    li.appendChild(span);
+    li.appendChild(plusButton);
+    cartItems.appendChild(li);
+    state.cart.push(item);
+    console.log(state.cart);
+    plusButton.addEventListener("click", () => {
+      item.quantity += 1;
+      span.innerText = item.quantity;
+      calculateTotal();
+    });
+
+    minusButton.addEventListener("click", () => {
+      item.quantity -= 1;
+      // state.cart.find((item) => {
+      //   if (item.quantity === 0) {
+      //     li.remove();
+      //     state.cart.splice(item);
+      //   }
+      // });
+      if (item.quantity < 1) {
+        let itemIndex;
+        for (let i = 0; i < state.cart.length; i++) {
+          if (state.cart[i].id === item.id) {
+            itemIndex = i;
+          }
+        }
+        li.remove();
+        state.cart.splice(itemIndex);
+      }
+      span.innerText = item.quantity;
+      calculateTotal();
+    });
+    calculateTotal();
   }
-
-  const li = document.createElement("li");
-  const img = document.createElement("img");
-  img.setAttribute("class", "cart--item-icon");
-  img.setAttribute("src", item.image);
-  img.setAttribute("alt", item.name);
-  const p = document.createElement("p");
-  p.innerText = item.name;
-  const minusButton = document.createElement("button");
-  minusButton.setAttribute("class", "remove-btn");
-  minusButton.innerText = "-";
-  const span = document.createElement("span");
-  span.innerText = "1";
-  const plusButton = document.createElement("button");
-  plusButton.setAttribute("class", "add-btn");
-  plusButton.innerText = "+";
-  li.appendChild(img);
-  li.appendChild(p);
-  li.appendChild(minusButton);
-  li.appendChild(span);
-  li.appendChild(plusButton);
-  cartItems.appendChild(li);
-  state.cart.push(item);
-  plusButton.addEventListener("click", () => {
-    item.quantity += 1;
-    span.innerText = item.quantity;
-  });
-  console.log(item.quantity);
-  minusButton.addEventListener("click", () => {
-    item.quantity -= 1;
-    if (item.quantity < 1) {
-      li.remove();
-      // li.innerHTML = "";
-    }
-    span.innerText = item.quantity;
-  });
 }
+function calculateTotal() {
+  let total = 0;
+  state.cart.forEach((item) => {
+    total = total + item.price * item.quantity;
+  });
+  const span = document.querySelector(".total-number");
+  span.innerText = "£" + Math.round(total * 100) / 100;
+}
+// let totalPricePerItem = 0;
+// for (let i = 0; i < state.cart.length; i++) {
+//   totalPricePerItem += state.items.price[i].count;
+//   return totalPricePerItem;
+//   console.log(price[i]);
+// }
 
-render();
+// function total() {
+//   let total = 0;
+//   state.cart.reduce((previusValue, currentValue) => {
+//     total = previusValue + currentValue;
+//     return total;
+//   }, 0);
+// }
+// console.log(calculateTotal());
+
+// console.log(state.cart);
+render(state.items);
