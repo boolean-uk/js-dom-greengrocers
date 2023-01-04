7bc123721c912e8fe280232623ef183ff43a0034
Oliver West <oliversimon.west@gmail.com>
2023-01-04 16:13:28 +0000

    Completed all extension requirements

--- index.js
+++ index.js
@@ -5,61 +5,61 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35,
+      price: 0.60,
       type: "vegetable"
     },
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35,
+      price: 0.20,
       type: "vegetable"
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35,
+      price: 1.00,
       type: "fruit"
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35,
+      price: 0.90,
       type: "fruit"
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35,
+      price: 0.80,
       type: "fruit"
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35,
+      price: 0.70,
       type: "fruit"
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35,
+      price: 0.50,
       type: "fruit"
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35,
+      price: 0.40,
       type: "fruit"
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35,
+      price: 0.30,
       type: "fruit"
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35,
+      price: 0.10,
       type: "vegetable"
     }
   ],
@@ -76,6 +76,25 @@ const sortDiv = document.querySelector(".filterAndSort")
 
 // PROGRAM LOGIC
 
+function compareNames(a, b) {
+    if ( a.name < b.name ){
+        return -1;
+      }
+      if ( a.name > b.name ){
+        return 1;
+      }
+      return 0;
+}
+
+function comparePrices(a, b) {
+    if ( a.price < b.price ){
+        return -1;
+      }
+      if ( a.price > b.price ){
+        return 1;
+      }
+      return 0;
+}
 
 // EVENT HANDLING
 
@@ -91,72 +110,6 @@ function basketChecker(item) {
         basketChecker.quantity++
         renderCartItems()
     }
-}
-
-// RENDERING
-function renderButtons() {
-    sortDiv.innerHTML = "";
-
-    const fruitButton = document.createElement("button")
-    fruitButton.innerText = "Fruit"
-
-    fruitButton.addEventListener("click", () => {
-        const fruitArray = state.items.filter((item) => item.type === "fruit")
-        console.log(`fruit array`, fruitArray)
-        console.log(`og array`, state.items)
-        renderItems(fruitArray)
-    })
-
-    const vegButton = document.createElement("button")
-    vegButton.innerText = "Vegetables"
-
-    vegButton.addEventListener("click", () => {
-        const vegArray = state.items.filter((item) => item.type === "vegetable")
-        console.log(`veg array`, vegArray)
-        console.log(`og array`, state.items)
-        renderItems(vegArray)
-    })
-
-    const allButton = document.createElement("button")
-    allButton.innerText = "All"
-
-    allButton.addEventListener("click", () => {
-        renderItems(state.items)
-    })
-
-    sortDiv.append(fruitButton, vegButton, allButton)
-}
-
-
-
-function renderItems(groceries) {
-    storeUL.innerHTML = "";
-
-    const foodItemsArray = groceries
-    foodItemsArray.forEach((item) => {
-        const li = document.createElement("li");
-        
-        const div = document.createElement("div");
-        div.setAttribute("class", "store--item-icon")
-        
-        const img = document.createElement("img");
-        img.setAttribute("src", `assets/icons/${item.id}.svg`)
-        img.setAttribute("alt", item.name)
-        
-        const bttn = document.createElement("button");
-        bttn.innerText = "Add to cart"
-        bttn.addEventListener("click", () => {
-            basketChecker(item)
-        })
-
-
-        li.append(div)
-        div.append(img)
-        li.append(bttn)
-        storeUL.append(li)
-    
-    })
-    renderButtons()
 }
 
 function renderCartItems() {
@@ -211,12 +164,92 @@ function renderCartItems() {
     renderTotalValue()
 }
 
+function renderButtons() {
+    sortDiv.innerHTML = "";
+
+    const fruitButton = document.createElement("button")
+    fruitButton.innerText = "Fruit"
+
+    fruitButton.addEventListener("click", () => {
+        const fruitArray = state.items.filter((item) => item.type === "fruit")
+        renderItems(fruitArray)
+    })
+
+    const vegButton = document.createElement("button")
+    vegButton.innerText = "Vegetables"
+
+    vegButton.addEventListener("click", () => {
+        const vegArray = state.items.filter((item) => item.type === "vegetable")
+        renderItems(vegArray)
+    })
+
+    const allButton = document.createElement("button")
+    allButton.innerText = "All"
+
+    allButton.addEventListener("click", () => {
+        // renders all items, from the original state.items array
+        renderItems(state.items)
+    })
+
+    const sortByPriceButton = document.createElement("button")
+    sortByPriceButton.innerText = "Sort By Price"
+
+    sortByPriceButton.addEventListener("click", () => {
+        const priceSort = state.items.sort(comparePrices)
+        renderItems(priceSort)
+    })
+
+    const sortByNameButton = document.createElement("button")
+    sortByNameButton.innerText = "Sort by Name"
+
+    sortByNameButton.addEventListener("click", () => {
+        const nameSort = state.items.sort(compareNames)
+        renderItems(nameSort)
+    })
+
+    sortDiv.append(fruitButton, vegButton, allButton, sortByNameButton, sortByPriceButton)
+}
+
+
+
+// RENDERING
+
+
+function renderItems(groceries) {
+    storeUL.innerHTML = "";
+
+    const foodItemsArray = groceries
+    foodItemsArray.forEach((item) => {
+        const li = document.createElement("li");
+        
+        const div = document.createElement("div");
+        div.setAttribute("class", "store--item-icon")
+        
+        const img = document.createElement("img");
+        img.setAttribute("src", `assets/icons/${item.id}.svg`)
+        img.setAttribute("alt", item.name)
+        
+        const bttn = document.createElement("button");
+        bttn.innerText = "Add to cart"
+        bttn.addEventListener("click", () => {
+            basketChecker(item)
+        })
+
+
+        li.append(div)
+        div.append(img)
+        li.append(bttn)
+        storeUL.append(li)
+    
+    })
+    renderButtons()
+}
+
+
 function renderTotalValue() {
     let total = 0
     state.cart.forEach((item) => {
-        const price = item.price
-        const quantity = item.quantity
-        const itemTotal = price * quantity
+        const itemTotal = item.price * item.quantity
         total += itemTotal
     })
     let totalPrice = total.toFixed(2)
