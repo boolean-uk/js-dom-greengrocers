0f883d74bf620cd50c8dda8cdddb21456fe5a217
Oliver West <oliversimon.west@gmail.com>
2023-01-04 12:29:41 +0000

    completed all core requirements

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// STATE
+
 const state = {
   items: [
     {
@@ -53,3 +55,126 @@ const state = {
   ],
   cart: []
 };
+
+
+// EXISTING HTML ELEMENT SELECTORS
+
+const storeUL = document.querySelector(".store--item-list")
+const cartUL = document.querySelector(".cart--item-list")
+const totalSpan = document.querySelector(".total-number")
+
+// PROGRAM LOGIC
+
+
+// EVENT HANDLING
+
+function basketChecker(item) {
+    
+    const basketChecker = state.cart.find(inBasket => inBasket.name === item.name)
+    if (basketChecker === undefined) {
+        const cartItem = {...item}
+        cartItem.quantity = 1
+        state.cart.push(cartItem)
+        renderCartItems()
+    } else {
+        basketChecker.quantity++
+        renderCartItems()
+    }
+}
+
+// RENDERING
+
+function renderItems() {
+    storeUL.innerHTML = "";
+
+    const foodItemsArray = state.items
+    foodItemsArray.forEach((item) => {
+        const li = document.createElement("li");
+        
+        const div = document.createElement("div");
+        div.setAttribute("class", "store--item-icon")
+        
+        const img = document.createElement("img");
+        img.setAttribute("src", `assets/icons/${item.id}.svg`)
+        img.setAttribute("alt", item.name)
+        
+        const bttn = document.createElement("button");
+        bttn.innerText = "Add to cart"
+        bttn.addEventListener("click", () => {
+            basketChecker(item)
+        })
+
+
+        li.append(div)
+        div.append(img)
+        li.append(bttn)
+        storeUL.append(li)
+    
+    })
+}
+
+function renderCartItems() {
+
+    cartUL.innerHTML = "";
+
+    state.cart.forEach((item) => {
+        const li = document.createElement("li");
+        
+        const img = document.createElement("img");
+        img.setAttribute("class", "cart--item-icon")
+        img.setAttribute("src", `assets/icons/${item.id}.svg`)
+        img.setAttribute("alt", item.name)
+    
+        const p = document.createElement("p")
+        p.innerText = item.name
+    
+        const bttnMinus = document.createElement("button")
+        bttnMinus.setAttribute("class", "quantity-btn remove-btn center")
+        bttnMinus.innerText = "-"
+        bttnMinus.addEventListener("click", (event) => {
+            item.quantity -= 1
+            // If the item quantity is = 0 after the decrement, then filter
+            // the cart, keeping only items that have a quantity greater than 0.
+            // then re-render the cart items.
+            if(item.quantity === 0){
+                state.cart = state.cart.filter((cart) => cart.quantity > 0)
+                renderCartItems()
+            }
+            span.innerText = item.quantity
+            renderTotalValue()
+        })
+    
+        const span = document.createElement("span")
+        span.setAttribute("class", "quantity-text center")
+        span.innerText = item.quantity
+    
+        const bttnAdd = document.createElement("button")
+        bttnAdd.setAttribute("class", "quantity-btn add-btn center")
+        bttnAdd.innerText = "+"
+        bttnAdd.addEventListener("click", () => {
+            item.quantity += 1
+            span.innerText = item.quantity
+            renderTotalValue()
+            
+        })
+    
+        li.append(img, p, bttnMinus, span, bttnAdd)
+        cartUL.append(li)
+
+    })
+    renderTotalValue()
+}
+
+function renderTotalValue() {
+    let total = 0
+    state.cart.forEach((item) => {
+        const price = item.price
+        const quantity = item.quantity
+        const itemTotal = price * quantity
+        total += itemTotal
+    })
+    let totalPrice = total.toFixed(2)
+    totalSpan.innerText = `£${totalPrice}`
+}
+
+renderItems()
