6f7193a285661d27454b4a10e7f3b0359b55d555
Chris Sach <christophersach90@gmail.com>
2023-01-04 10:27:21 +0000

    store items done, shopping list next, then buttons

--- index.js
+++ index.js
@@ -53,3 +53,40 @@ const state = {
   ],
   cart: []
 };
+
+// Element selectors
+
+const shopItemList = document.querySelector(".store--item-list");
+const cartItemList = document.querySelector(".cart--item-list");
+
+// Program Logic
+
+// Event Handling
+
+// Rendering
+
+function renderShop () {
+  state.items.forEach((item) => {
+    const li = document.createElement('li');
+    
+    const div = document.createElement('div');
+    div.setAttribute("class", "store--item-icon");
+
+    const img = document.createElement('img');
+    img.src = 'assets/icons/' + item.id + '.svg';
+    img.alt = item.name;
+    
+    const addToCart = document.createElement('button');
+    addToCart.innerHTML = 'Add to cart'
+
+    div.append(img);
+    li.append(div);
+    li.append(addToCart);
+
+    shopItemList.append(li);
+
+
+  })
+}
+
+renderShop()
