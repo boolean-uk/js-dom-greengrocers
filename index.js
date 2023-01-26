db1c10a6de6c63045172d79126491fe8d4cee96b
James Byrne <james.c.byrne1994@gmail.com>
2023-01-26 12:13:47 +0000

    core criteria met

--- index.js
+++ index.js
@@ -59,11 +59,7 @@ const greenGrocers = document.querySelector('.store--item-list' )
 const userCart = document.querySelector('.cart--item-list')
 
 const itemArray = state.items
-//console.log('test array ',itemArray )
-
-//const fruitAndVegName = state.items[i].name
-//console.log('test fruit ', fruitAndVegName)
-
+const cartArray = state.cart
 
 const createLi = () => {
   for(let i = 0; i < itemArray.length; i++){
@@ -71,83 +67,118 @@ const createLi = () => {
     const theDiv = document.createElement('div')
     const foodButton = document.createElement("button");
     const imageOfFood = document.createElement('img')
-
-    foodButton.textContent = ' add to cart'
-    //console.log(foodButton)
     theDiv.classList = "store--item-icon"
-    // const foodDiv = document.createElement('div')
     greenGrocers.append(myLi)
     myLi.append(theDiv)
-
     const fruitAndVegId = state.items[i].id
-    
-    //console.log(fruitAndVegId)
-     imageOfFood.setAttribute('src', "/assets/icons/" + fruitAndVegId + ".svg" )
-    // console.log(" the image here is ", imageOfFood)
+    imageOfFood.setAttribute('src', "/assets/icons/" + fruitAndVegId + ".svg" )
     theDiv.append(imageOfFood)
     myLi.append(foodButton)
-  }
 
+    foodButton.textContent = ' add to cart'
+
+    foodButton.addEventListener('click', () => {
+      const item = itemArray[i];
+      if(cartArray.some((x) => x.id === item.id)) {
+        cartArray[cartArray.indexOf(item)].quantity++;
+        cartArray[cartArray.indexOf(item)].price =
+        cartArray[cartArray.indexOf(item)].price ;
+        createCartItem()
+        console.log(cartArray);
+      } else {
+        item.quantity = 1;
+        cartArray.push(item);
+        createCartItem()
+        console.log(cartArray);
+      }
+
+    })
+  }
 }
 
-createLi()
+let totalCost = 0;
 
-
-function cartForLoop() {
-  for(let i = 0; i < itemArray.length; i++){
-
+const createCartItem = () => {
+  userCart.innerHTML = "";
+  totalCost = 0;
+  if(cartArray.length === 0){
+    totalCost = 0;
+    total.innerHTML = totalCost.toFixed(2);
+    return;
+  }
+  cartArray.forEach((item, index) => {
   const cartLi = document.createElement('li')
   const imageOfFoodInCart = document.createElement('img')
   const foodName = document.createElement('p')
   const removeBtn = document.createElement('button')
   const quantitySpan = document.createElement('span')
   const addBtn = document.createElement('button')
+  
 
-  removeBtn.classList = 'quantity-btn', 'remove-btn center'
+  removeBtn.classList = 'quantity-btn remove-btn center'
+  
   removeBtn.textContent = '-'
 
-  addBtn.classList = 'quantity-btn', 'add-btn center'
-  addBtn.textContent = '+'
 
-  quantitySpan.textContent= '1'
+  removeBtn.addEventListener('click', () => {
+    item.quantity--;
+    totalCost -= item.price * item.quantity;
+    total.innerHTML = totalCost.toFixed(2);
+    if(item.quantity === 0){
+      cartArray.splice(index,1);
+      createCartItem(); 
+    }
+    else{
+      quantitySpan.innerHTML = item.quantity;
+    }
+  });
+
+
+  addBtn.addEventListener('click', () => {
+    item.quantity++;
+    totalCost += item.price * item.quantity;
+    total.innerHTML = totalCost.toFixed(2);
+    quantitySpan.innerHTML = item.quantity;
+  });
+
+
   
-  foodName.textContent = state.items[i].name
-
-
-  const foodId = state.items[i].id
-
+  addBtn.classList = 'quantity-btn add-btn center'
+  addBtn.textContent = '+'
+  
+  quantitySpan.innerHTML= item.quantity
+  
+  foodName.textContent = state.cart[index].name
+  
+  
+  const foodId = item.id
+  
   imageOfFoodInCart.classList = "cart--item-icon"
   imageOfFoodInCart.setAttribute('src', "/assets/icons/" + foodId + ".svg" )
-  //console.log(imageOfFoodInCart)
   
+  userCart.append(cartLi)
+  cartLi.append(imageOfFoodInCart, foodName, removeBtn,quantitySpan,addBtn)
+
+  const total = document.querySelector(".total-number");
+  totalCost += item.price * item.quantity; 
+  // totalCost += item.price;
+
+  total.innerHTML = totalCost.toFixed(2)
   
 
+  console.log('here is the total ' , total)
 
-    userCart.append(cartLi)
-    cartLi.append(imageOfFoodInCart, foodName, removeBtn,quantitySpan,addBtn)
+
+
+
+
+  });
   }
- 
+
+const init = () => {
+  //cartForLoop()
+  createLi()
+  createCartItem()
 }
-cartForLoop()
 
-
-// function renderCart() {
-//   state.cart.forEach( item => {
-//     //create cart elements for each item and appent them to the cart ul
-//     const cartLi = document.createElement('li')
-//     console.log('this is an', cartLi)
-//     userCart.append(cartLi)
-//   })  
-//  }
-
-//  renderCart(state) 
- 
-
-//  const addToCartButton = document.querySelector('button')
-
-//  addToCartButton.addEventListener('click', function(){
-//   alert('hello')
-//   //  userCart.push(item)
-//   //  renderCart()
-//  })
-
+init()
