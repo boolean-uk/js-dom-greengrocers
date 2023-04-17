7744d86648dcf713054b354806288e88798f7018
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2023-04-17 16:28:02 +0100

    rendering store items and cart

--- index.js
+++ index.js
@@ -53,3 +53,67 @@ const state = {
   ],
   cart: []
 };
+
+// rendering store items
+const storeUl = document.querySelector('.store--item-list')
+const cartUl = document.querySelector('.cart--item-list')
+console.log(cartUl)
+
+
+
+function renderAllStoreItems(){
+  for(let i = 0; i < state.items.length; i++){
+    const storeLi = document.createElement('li')
+    storeUl.appendChild(storeLi)
+    
+    const storeLiIconDiv = document.createElement('div')
+    storeLiIconDiv.setAttribute('class', 'store--item-icon')
+    storeLi.appendChild(storeLiIconDiv)
+    
+    const storeLiImg = document.createElement('img')
+    storeLiImg.setAttribute('src', `assets/icons/${state.items[i].id}.svg`)
+    storeLiImg.setAttribute('alt', `${state.items[i].name}`)
+    storeLiIconDiv.appendChild(storeLiImg)
+    
+    const storeLiBtn = document.createElement('button')
+    storeLiBtn.innerText = 'Add to cart'
+    storeUl.appendChild(storeLiBtn)
+  }
+}
+
+// rendering cart items
+
+const cartLi = document.createElement('li')
+cartUl.appendChild(cartLi)
+
+const cartLiImg = document.createElement('img')
+cartLiImg.setAttribute('class', 'cart--item-icon')
+cartLiImg.setAttribute('src', 'assets/icons/001-beetroot.svg')
+cartLiImg.setAttribute('alt', 'beetroot')
+cartLi.appendChild(cartLiImg)
+
+const p = document.createElement('p')
+cartLi.appendChild(p)
+p.innerText = 'beetroot'
+
+const removeBtn = document.createElement('button')
+removeBtn.setAttribute('class', 'remove-btn')
+removeBtn.innerText = '-'
+cartLi.appendChild(removeBtn)
+
+const qntSpan = document.createElement('span')
+qntSpan.setAttribute('class', 'quantity-text')
+qntSpan.innerText = 1
+cartLi.appendChild(qntSpan)
+
+const addBtn = document.createElement('button')
+addBtn.setAttribute('class', 'add-btn')
+addBtn.innerText = '+'
+cartLi.appendChild(addBtn)
+
+
+
+
+
+
+renderAllStoreItems()
