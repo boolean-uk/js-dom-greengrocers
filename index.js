d81dde902678e98d0c20fd66414a3eebab44d7d0
Priya <ps975076@gmail.com>
2023-04-18 14:31:53 +0100

    some work done

--- index.js
+++ index.js
@@ -53,3 +53,48 @@ const state = {
   ],
   cart: []
 };
+
+// 1. Creating a function for all the list of items - pulling data from above state.items list
+// 2.  Create a const for header and selecting header 
+// 3. Create a const for ul and selecting the ul
+// 4. Creating a variable called li and then creating an element within ul and header.
+// 5. Appending li to ul
+
+function listOfItems(){
+  for (let i = 0; i < state.items.length; i++) {
+const header = document.querySelector("#store")
+const ul = document.querySelector('.item-list')
+console.log(ul)
+const li = document.createElement('li')
+ul.append(li)
+
+
+// Creating the div inside the Li
+// 1. Create variable for div - create an element for div
+// 2. Set class attribute for div
+// 3. Append div to the li
+// 4. Create image element inside of div
+// 5 . Set attribute - icons
+// 6. Append the image to the div
+
+  const div = document.createElement("div")
+  div.setAttribute("class", "store--item-icon")
+  li.append(div)
+  const img = document.createElement('img')
+  img.setAttribute('src','assets/icons/001-beetroot.svg')
+  div.append(img)
+
+  // Button - Add to cart
+  // 1. Create an element call button - this is within li
+  // 2. set innertexr to button "add to cart"
+  // 3. Append the button to the li
+
+  const button = document.createElement("button")
+  button.innerText = "add to cart"
+  li.append(button)
+
+ 
+
+}
+}
+listOfItems()
