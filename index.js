a30b4ab56c9fec0109606f3b1685531355c39ea3
Ian Grantham <dev.iangrantham@gmail.com>
2023-04-18 11:54:36 +0100

    add to cart event listener corrected, comments tidied up

--- index.js
+++ index.js
@@ -7,8 +7,10 @@
 
 // SELECTING EXISTING ELEMENTS
   // store ul - class=store--item-list
-const selectStoreStockUl = document.querySelector('.store--item-list')
+const selectStoreUl = document.querySelector('.store--item-list')
   // cart ul - cart--item-list
+const selectCartUl = document.querySelector('.cart--item-list')
+
 
 
 // STATE
@@ -70,12 +72,55 @@ const state = {
 
 // EVENT LISTENERS
 
+// From the store, a user can add an item to their cart
+
+
+function test() {
+  console.log('test')
+}
+
+function listenForAdd() {
+// when button is clicked, adds the item to the cart in the state
+  console.log('called: listenForAdd')
+// listen for add to cart button press
+  const allAddButtons = document.querySelectorAll('.store-button')
+  allAddButtons.forEach((allAddButtons) => {
+  allAddButtons.addEventListener('click', (e) => {
+// identify which button
+    const whichAddButton = e.target.id
+    console.log('button clicked:', whichAddButton)
+  })
+})
+
+  
+  // push that info to cart
+    // cart location = state.cart = []
+
+
+
+}
+
+
+
+
+
+
+
+  // identify which button has been clicked
+  // use that info to copy the relevant item into the cart
+
+
+
+
+
+
 
 // RENDER LOGIC
 
 // A user can view a selection of items in the store
   // display state items in greengrocers section
 
+// RENDER STORE
 // make var to select array in state
 const stateItems = state.items
 console.log('stateItems', stateItems)
@@ -85,40 +130,64 @@ function renderStore() {
   // check if called
   console.log('called: renderStore')
   // clear the ul 
-  selectStoreStockUl.innerHTML = ''
+  selectStoreUl.innerHTML = ''
 
   // create all li based on state
   for (let i = 0; i < stateItems.length; i++) {
   // var to store the state item being looked at
-    const stockItem = stateItems[i]
+    const storeItem = stateItems[i]
   // make new li
-    const makeStoreStockLi = document.createElement('li')
+    const makeStoreLi = document.createElement('li')
   // append li to ul
-    selectStoreStockUl.appendChild(makeStoreStockLi)
+    selectStoreUl.appendChild(makeStoreLi)
 
   // create a div, set class="store--item-icon", append to li
-    const makeStoreStockDiv = document.createElement('div')
-    makeStoreStockDiv.setAttribute('class', 'store--item-icon')
-    makeStoreStockLi.appendChild(makeStoreStockDiv)
+    const makeStoreDiv = document.createElement('div')
+    makeStoreDiv.setAttribute('class', 'store--item-icon')
+    makeStoreLi.appendChild(makeStoreDiv)
 
     // create an img, set src and alt, append to div
-      const makeStoreStockImg = document.createElement('img')
-      const stockImgSrc = stockItem.id
-      makeStoreStockImg.setAttribute('src', `assets/icons/${stockImgSrc}.svg`)
-      const stockImgAlt = stockItem.name
-      makeStoreStockImg.setAttribute('alt', `${stockImgAlt}`)
-      makeStoreStockDiv.appendChild(makeStoreStockImg)
+      const makeStoreImg = document.createElement('img')
+      const storeImgSrc = storeItem.id
+      makeStoreImg.setAttribute('src', `assets/icons/${storeImgSrc}.svg`)
+      const storeImgAlt = storeItem.name
+      makeStoreImg.setAttribute('alt', `${storeImgAlt}`)
+      makeStoreDiv.appendChild(makeStoreImg)
 
     // create a button, set button text, append to li
-    const storeStockButton = document.createElement('button')
-    storeStockButton.innerText = 'Add to cart'
-    makeStoreStockLi.append(storeStockButton)
- 
+    const storeButton = document.createElement('button')
+    storeButton.setAttribute('class', 'store-button')
+    storeButton.setAttribute('id', `${storeImgAlt}`)
+    storeButton.innerText = 'Add to cart'
+    makeStoreLi.append(storeButton)
   }
+
+  listenForAdd()
 }
+
+
+// RENDER CART
+    // find where added item should go (cart--item-list)
+
+// Cart template:
+
+{/* <li>
+  <img
+    class="cart--item-icon"
+    src="assets/icons/001-beetroot.svg"
+    alt="beetroot"
+  />
+  <p>beetroot</p>
+  <button class="quantity-btn remove-btn center">-</button>
+  <span class="quantity-text center">1</span>
+  <button class="quantity-btn add-btn center">+</button>
+</li> */}
+
+
 
 // FIRST PAGE LOAD LOGIC
 function init() {
   renderStore()
+ 
 }
 init()
