a09edf1d6d6049827a8a52d7dab5bc412ae09265
Chris Sach <christophersach90@gmail.com>
2023-04-18 14:19:56 +0100

    store items rendered

--- index.js
+++ index.js
@@ -53,3 +53,30 @@ const state = {
   ],
   cart: []
 };
+
+const storeList = document.querySelector('.store--item-list');
+
+function renderStoreList() {
+  const storeItems = state.items
+
+  for (let i = 0; i < storeItems.length; i++) {
+    let item = storeItems[i];
+
+    const storeListItem = document.createElement('li');
+    storeList.append(storeListItem);
+
+    const storeIconContainer = document.createElement('div');
+    storeIconContainer.setAttribute('class', 'store--item-icon');
+    storeListItem.append(storeIconContainer)
+
+    const storeIcon = document.createElement('img');
+    storeIcon.setAttribute('src', `assets/icons/${item.id}.svg`)
+    storeIconContainer.append(storeIcon);
+
+    const storeButton = document.createElement('button');
+    storeButton.innerText = 'Add to cart'
+    storeListItem.append(storeButton)
+  }
+}
+
+renderStoreList()
