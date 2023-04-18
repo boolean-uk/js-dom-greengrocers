30302157510e2b8871a83bcc24c8a0a6dd14314d
Jay Vithani <jvithani@hotmail.com>
2023-04-18 12:20:22 +0100

    Created plan

--- index.js
+++ index.js
@@ -53,3 +53,54 @@ const state = {
   ],
   cart: []
 };
+
+// ---------------CORE REQUIREMENTS PLAN ----------//
+// - Implement the following requirements
+  - A user can view a selection of items in the store
+  
+  
+  // create some array of items
+  // create function to select item
+  // append item to store
+  // create loop to add new items
+  // make new items into a list
+  
+  - From the store, a user can add an item to their cart
+
+  // create function to pick item from above list
+  // append new item to cart list
+
+    - If the item is already in the cart, increase the item's quantity in the cart
+
+    // find duplicate item in cart
+    // increase item amount by one for each duplicate (for loop here?)
+
+
+  - From the cart, a user can view and adjust the number of items in their cart
+
+  // add click event for additional item
+  // add click event for reomval of item
+  // can above 2 steps be made into one function with else/if statements??
+
+
+      - If an item's quantity equals zero it is removed from the cart
+
+      // add if statement that will clear cart but only selected item
+
+  - A user can view the current total in their cart
+
+  // add click event AND/OR function that returns sum of cart items
+
+// ---------------------- EXTENSION ONE ----------------//
+
+- Add filters to the store ie. filter by item type; when a user clicks a filter they will only see items of that type
+
+// add click event function that will recognise item
+// append item to new "selected" list
+
+
+// ----------------------- EXTENSION 2 ------------------//
+
+- Add sorting to the store ie. sort by price or sort alphabetically; when a user clicks sort they will see a sorted list of items
+
+// add click event function to arrange "ordered list" numerically "pushing" highest value first going to lowest in "array"???
