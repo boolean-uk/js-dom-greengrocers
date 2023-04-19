9bcda706e5188fcbcf46166e0de50fc8cd958504
Timmyctt <timmyc1990@yahoo.com>
2023-04-19 17:04:59 +0100

    completed core

--- js.js
+++ js.js
@@ -1,9 +1,18 @@
-//Create store items
-function create_store_items() {
+let cart = [];
+//copy list items from store to cart & add quantity key value = 0 to start
+for (let k = 0; k < state.items.length; k++) {
+  cart.push(state.items[k]);
+  cart[k].quantity = 0;
+}
+
+displayStore();
+getTotal();
+
+function displayStore() {
   for (let i = 0; i < state.items.length; i++) {
     let id = state.items[i].id;
     let name = state.items[i].name;
-    let price = state.items[i].price;
+    /* let price = state.items[i].price; */
     let imgpath = `assets/icons/${id}.svg`;
 
     const itemList_store = document.querySelector(".store--item-list");
@@ -19,24 +28,81 @@ function create_store_items() {
     const button = document.createElement("button");
     button.innerText = "Add to cart";
     storeItem.append(button);
-
     button.setAttribute("id", `${id}`);
     button.addEventListener("click", function () {
-      addtocart(id, name, price, imgpath);
+      addToCart(i);
+      resetCart();
+      displayCart();
+      getTotal();
     });
   }
 }
 
-function addtocart(id, name, price, imgpath) {
-  console.log(`${name} costs ${price} ${id} ${imgpath}`);
-  /*  const itemlist_cart = document.querySelector(".cart--item-list");
-  const add_item_cart = document.createElement("li");
-  itemlist_cart.append(add_item_cart);
-  const img = document.createElement("img");
-  add_item_cart.append(img);
-  img.setAttribute("class", "cart--item-icon");
-  img.setAttribute("src", "assets/icons/001-beetroot.svg");
-  img.setAttribute("alt", "beetroot"); */
+function addToCart(i) {
+  cart[i].quantity += 1;
 }
 
-create_store_items();
+function resetCart() {
+  const itemList_cart = document.querySelector(".cart--item-list");
+  itemList_cart.innerText = "";
+}
+
+function displayCart() {
+  for (let j = 0; j < cart.length; j++) {
+    if (cart[j].quantity > 0) {
+      let id = cart[j].id;
+      let name = cart[j].name;
+      /* let price = cart[j].price; */
+      let imgpath = `assets/icons/${id}.svg`;
+      let quantity = cart[j].quantity;
+      const itemList_cart = document.querySelector(".cart--item-list");
+      const cartItem = document.createElement("li");
+      itemList_cart.append(cartItem);
+      const img = document.createElement("img");
+      cartItem.append(img);
+      img.setAttribute("class", "cart--item-icon");
+      img.setAttribute("src", `${imgpath}`);
+      img.setAttribute("alt", `${name}`);
+      const p = document.createElement("p");
+      cartItem.append(p);
+      p.innerText = `${name}`;
+      const buttonMinus = document.createElement("button");
+      cartItem.append(buttonMinus);
+      buttonMinus.innerText = "-";
+      buttonMinus.setAttribute("class", "quantity-btn remove-btn center");
+      const span = document.createElement("span");
+      span.innerText = `${quantity}`;
+      span.setAttribute("class", "quantity-text center");
+      cartItem.append(span);
+      const buttonPlus = document.createElement("button");
+      cartItem.append(buttonPlus);
+      buttonPlus.innerText = "+";
+      buttonPlus.setAttribute("class", "quantity-btn add-btn center");
+      buttonPlus.addEventListener("click", function () {
+        cart[j].quantity += 1;
+        span.innerText = `${cart[j].quantity}`;
+        resetCart();
+        displayCart();
+        getTotal();
+      });
+      buttonMinus.addEventListener("click", function () {
+        cart[j].quantity -= 1;
+        span.innerText = `${cart[j].quantity}`;
+        resetCart();
+        displayCart();
+        getTotal();
+      });
+    }
+  }
+}
+
+function getTotal() {
+  let total = 0;
+  for (let l = 0; l < cart.length; l++) {
+    let quantity = cart[l].quantity;
+    let price = cart[l].price;
+    total += quantity * price;
+    const totalNum = document.querySelector(".total-number");
+    totalNum.innerText = `£${total.toFixed(2)}`;
+  }
+}
