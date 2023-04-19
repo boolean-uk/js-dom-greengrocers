e0ff138ad6f3da60478d5e7390af730390792702
Koci H <husnukoci94@gmail.com>
2023-04-19 08:26:22 +0300

    Work on it

--- index.js
+++ index.js
@@ -53,3 +53,101 @@ const state = {
   ],
   cart: []
 };
+const shop = document.querySelector(".store--item-list");
+const cart = document.querySelector(".cart--item-list");
+const addedPrice = document.querySelector(".added-price");
+
+function showShop(){
+  shop.innerHTML = ""
+  state.items.forEach((item) => {
+    // console.log("eachOne", item)
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.setAttribute("class", "store--item-icon");
+    const img = document.createElement("img");
+    img.src = `assets/icons/${item.id}.svg`;
+    img.alt = `${item.name}`;
+    const addButton = document.createElement("button");
+    addButton.innerText = "add to cart";
+    addButton.addEventListener("click", function (e) {
+      console.log("here is the id", item.id);
+
+    if (!item.quantity) {
+      console.log("which boolean", !item.quantity);
+      console.log("quantity", item.quantity);
+      item.quantity = 0;
+      console.log("here", item.quantity)
+    }
+
+    item.quantity += 1;
+
+    if (!state.cart.find(({id}) => id === item.id)) {
+    
+      console.log("it must be from here", !state.cart);
+      state.cart.push(item);
+
+    }
+  
+    cartItem();
+
+  });
+  li.append(div, addButton);
+  div.append(img);
+  shop.append(li);
+  });
+}
+showShop()
+
+function cartItem() {
+  let totalPrice = 0;
+  cart.innerText = "";
+  state.cart.forEach((item) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.setAttribute("class", "cart--item-icon");
+    img.src = `assets/icons/${item.id}.svg`;
+    img.alt = `${item.name}`;
+    const p = document.createElement("p");
+    p.innerText = `${item.name}`;
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "quantity-btn remove-btn center");
+    removeButton.innerText = "-";
+    totalPrice += item.quantity * 0.35;
+    removeButton.addEventListener("click", () => {
+      item.quantity--;
+      
+      
+      if (item.quantity <= 0) {
+        
+        state.cart = state.cart.filter((decreasingItem) => decreasingItem.id !== item.id
+          );
+        }
+
+        cartItem();
+      });
+      const span = document.createElement("span");
+      span.setAttribute("class", "quantity-text center");
+      span.innerText = item.quantity;
+      const addButton = document.createElement("button");
+      addButton.setAttribute("class", "quantity-btn add-btn center");
+      addButton.innerText = "+";
+      addButton.addEventListener("click", () => {
+        item.quantity++;
+        console.log("add button", item.quantity)
+        cartItem();
+      });
+      
+      cart.append(li);
+      li.append(img, p, removeButton, span, addButton);
+      return li;
+    });
+    
+    calculate(totalPrice);
+  }
+  cartItem();
+  
+  function calculate(totalPrice) {
+    addedPrice.innerText = "£" + totalPrice.toFixed(2);
+  }
+  // Gonna add filter
+  
