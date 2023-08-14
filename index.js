d799a20062248990dcb09cdb5ac545eeec2ca389
iliask796 <iliaskal7@gmail.com>
2023-08-14 13:47:39 +0300

    Added Icons+Buttons in the Store Display

--- index.js
+++ index.js
@@ -53,3 +53,33 @@ const state = {
   ],
   cart: []
 };
+
+const groceries = document.querySelector('.store--item-list')
+
+function displayStore() {
+  const storeItems = state.items
+  for (let i = 0; i < storeItems.length; i++){
+    //Create Vegetable Icon in the Store Section
+    const storeItem = document.createElement('li')
+    const vegetable = document.createElement('div')
+    vegetable.classList.add("store--item-icon")
+    const img = document.createElement('img')
+    img.src = 'assets/icons/' + storeItems[i].id + '.svg'
+    img.alt = storeItems[i].name
+
+    //Create Button below each Vegetable Icon
+    const button = document.createElement('button')
+    button.innerText = 'Add to cart'
+    button.addEventListener('click', function() {
+      console.log(storeItems[i].name)
+    })
+
+    //Append Elements to the page
+    vegetable.append(img)
+    storeItem.append(vegetable)
+    storeItem.append(button)
+    groceries.append(storeItem)
+  }
+}
+
+displayStore()
