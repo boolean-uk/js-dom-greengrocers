55cde3fb1c202e0fc36839cb58176b37766d1850
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-10-13 15:33:38 +0100

    render store 1st iteration done

---
+++ constants.js
@@ -0,0 +1,64 @@
+const state = {
+  items: [
+    {
+      id: "001-beetroot",
+      name: "beetroot",
+      price: 0.35
+    },
+    {
+      id: "002-carrot",
+      name: "carrot",
+      price: 0.35
+    },
+    {
+      id: "003-apple",
+      name: "apple",
+      price: 0.35
+    },
+    {
+      id: "004-apricot",
+      name: "apricot",
+      price: 0.35
+    },
+    {
+      id: "005-avocado",
+      name: "avocado",
+      price: 0.35
+    },
+    {
+      id: "006-bananas",
+      name: "bananas",
+      price: 0.35
+    },
+    {
+      id: "007-bell-pepper",
+      name: "bell pepper",
+      price: 0.35
+    },
+    {
+      id: "008-berry",
+      name: "berry",
+      price: 0.35
+    },
+    {
+      id: "009-blueberry",
+      name: "blueberry",
+      price: 0.35
+    },
+    {
+      id: "010-eggplant",
+      name: "eggplant",
+      price: 0.35
+    }
+  ],
+  cart: []
+};
+
+const STORE = document.querySelector("#store");
+const STORE_ITEM_LIST = STORE.querySelector(".store--item-list")
+
+const CART = document.querySelector("#cart");
+const CART_ITEM_LIST = CART.querySelector(".cart--item-list");
+
+const TOTAL_SECTION = document.querySelector(".total-section");
+const TOTAL_NUMBER = document.querySelector(".total-number");
--- render.js
+++ render.js
@@ -1,11 +1,28 @@
+// Render
+
 function renderAll() {
   renderStore();
   renderCart();
 }
 
-function renderStore() {}
+function renderStore() {
+  state.items.forEach((item) => {
+    renderStoreItems(item);
+  });
+}
 
-function renderStoreItems() {}
+function renderStoreItems(item) {
+  const li = createElement("li");
+  const itemIconDiv = createElement("div", "store--item-icon");
+  const img = createImg(item);
+  const button = createElement("button", null, null, "Add to cart");
+
+  li.append(itemIconDiv);
+  itemIconDiv.append(img);
+  li.append(button);
+
+  STORE_ITEM_LIST.append(li);
+}
 
 function renderCart() {}
 
@@ -13,5 +30,30 @@ function renderCartItems() {}
 
 function renderTotal() {}
 
+// Clear
+
 function clearElement() {}
 
+// Create
+
+function createElement(elementName, elementClass, elementId, innerText) {
+  const element = document.createElement(elementName);
+
+  elementClass ? (element.className = elementClass) : null;
+  elementId ? (element.id = elementId) : null;
+  innerText ? (element.innerText = innerText) : null;
+
+  return element;
+}
+
+function createImg(item) {
+  const img = createElement("img");
+
+  img.src = `assets/icons/${item.id}.svg`;
+  img.alt = item.name;
+
+  return img;
+}
+
+const BEETROOT = state.items[0];
+renderStore();
