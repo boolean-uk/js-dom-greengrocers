7143979db36f7009b4d186a81d31069e2ef13e71
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-10-13 15:54:38 +0100

    render cart 1st iteration

--- render.js
+++ render.js
@@ -17,16 +17,38 @@ function renderStoreItems(item) {
   const img = createImg(item);
   const button = createElement("button", null, null, "Add to cart");
 
-  li.append(itemIconDiv);
+  multiAppend(li, itemIconDiv, button);
   itemIconDiv.append(img);
-  li.append(button);
-
   STORE_ITEM_LIST.append(li);
 }
 
-function renderCart() {}
+function renderCart() {
+  state.cart.forEach((item) => {
+    renderCartItems(item)
+  });
+}
 
-function renderCartItems() {}
+function renderCartItems(item) {
+  const li = createElement("li");
+  const img = createImg(item, "cart--item-icon");
+  const p = createElement("p", null, null, item.name);
+  const buttonRemove = createElement(
+    "button",
+    "quantity-btn remove-btn center",
+    null,
+    "-"
+  );
+  const span = createElement("span", "quantity-text center", null, "1");
+  const buttonAdd = createElement(
+    "button",
+    "quantity-btn add-btn center",
+    null,
+    "+"
+  );
+
+  multiAppend(li, img, p, buttonRemove, span, buttonAdd);
+  CART_ITEM_LIST.append(li);
+}
 
 function renderTotal() {}
 
@@ -46,14 +68,30 @@ function createElement(elementName, elementClass, elementId, innerText) {
   return element;
 }
 
-function createImg(item) {
+function createImg(item, className) {
   const img = createElement("img");
 
+  className ? (img.className = className) : null;
   img.src = `assets/icons/${item.id}.svg`;
   img.alt = item.name;
 
   return img;
 }
 
+// append
+
+function multiAppend(parentElement, ...childElements) {
+  childElements.forEach((child) => {
+    parentElement.append(child);
+  });
+}
+
 const BEETROOT = state.items[0];
 renderStore();
+
+state.cart.push(BEETROOT)
+state.cart.push(BEETROOT)
+state.cart.push(BEETROOT)
+state.cart.push(BEETROOT)
+state.cart.push(BEETROOT)
+renderCart()
