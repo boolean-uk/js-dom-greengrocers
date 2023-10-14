4216942a9d8c3602688a8f4cd74bfdccef419bdb
TomEastwood <t.eastwood99@gmail.com>
2023-10-14 11:32:02 +0100

    created item cards in main section

--- index.js
+++ index.js
@@ -53,3 +53,34 @@ const state = {
   ],
   cart: []
 };
+
+//const for the product section
+const productSection = document.querySelector(".store--item-list")
+
+//function to create the cards
+function createCard(){
+  state.items.forEach(item => {
+    // Create elements for the item card
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const button = document.createElement('button');
+    button.innerText = "Add to cart";
+
+    img.src = `assets/icons/${item.id}.svg`;
+    div.setAttribute('class', 'store--item-icon');
+    div.append(img);
+
+    li.append(div, button);
+    productSection.append(li);
+  });
+}
+createCard();
+  
+//function to add items to cart
+
+//function to check if the item is in cart and if so increase the quantity in the cart
+
+//function to look inside the cart and adjust the number of items inside, if the quantity is 0, remove the item from the cart
+
+//function to calculate the total price of the items in the cart
