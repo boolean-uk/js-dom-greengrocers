a45952f274db9ac366b3fd301a2a52f82c05a015
Rebecca Noy <rebecca.noy@gmx.de>
2023-10-14 11:27:47 +0200

    added displayItems() function

--- index.js
+++ index.js
@@ -53,3 +53,40 @@ const state = {
   ],
   cart: []
 };
+
+const displayItems = () => {
+  const itemDisplaySection = document.querySelector("header#store ul.item-list")
+
+  state.items.forEach((item) => {
+    const li = document.createElement("li")
+    
+    const img = document.createElement("img")
+    img.class = "cart-item-icon"
+    img.src = "assets/icons/" + item.id + ".svg"
+    img.alt = item.val
+    
+    const p = document.createElement("p")
+    p.innerText = item.name
+    li.appendChild(img)
+    li.appendChild(p)
+
+    const buttonRemove = document.createElement("button")
+    buttonRemove.class = "quantity-btn remove-btn center"
+    buttonRemove.innerText = "-"
+    li.appendChild(buttonRemove)
+    
+    const quantity = document.createElement("span")
+    quantity.class = "quantity-text center"
+    quantity.innerText = 1
+    li.appendChild(quantity)
+    
+    const buttonAdd = document.createElement("button")
+    buttonAdd.class = "quantity-btn add-btn center"
+    buttonAdd.innerText = "+"
+    li.appendChild(buttonAdd)
+
+    itemDisplaySection.appendChild(li)
+  })
+}
+
+displayItems()
