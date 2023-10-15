84156e2eb52781a7f557ada01e5a4888c0f77a85
Satoki <satokiito@gmail.com>
2023-10-15 18:54:50 +0100

    cart rendered - currently static

--- index.js
+++ index.js
@@ -54,35 +54,56 @@ const state = {
   cart: []
 };
 
-// HIGH LEVEL STORE VARIABLES
+// HIGH LEVEL VARIABLES
 const storeList = document.querySelector('#store > ul')
+const cartList = document.querySelector('#cart > div > ul')
 
-// COMPLETE RENDER FUNCTION
-
+// FULL PAGE RENDER FUNCTION
 const render = () => {
   renderStore()
+  renderCart()
 }
 
-
+// STORE
 // CREATE STORE FUNCTIONS
-const createStoreItemButton = () => {
-  const storeItemButton = document.createElement('button')
-  storeItemButton.innerText = "Add to cart"
-  return storeItemButton
+const renderStoreItemDiv = (item) => {
+  const storeItemDiv = document.createElement('div')
+  storeItemDiv.setAttribute('class', 'store--item-icon')
+  storeItemDiv.append(renderStoreItemImage(item))
+  return storeItemDiv
 }
 
-const selectStoreItemImage = (item) => {
+const renderStoreItemImage = (item) => {
   const storeItemImage = document.createElement('img')
   storeItemImage.src = `assets/icons/${item.id}.svg`
+  storeItemImage.alt = item.name
   return storeItemImage
 }
 
-const createStoreItemDiv = (item) => {
-  const storeItemDiv = document.createElement('div')
-  storeItemDiv.setAttribute('class', 'store--item-icon')
-  storeItemDiv.append(selectStoreItemImage(item))
+const renderStoreItemButton = (item) => {
+  const storeItemButton = document.createElement('button')
+  storeItemButton.innerText = "Add to cart"
 
-  return storeItemDiv
+  storeItemButton.addEventListener('click', (e) => {
+
+    renderCart()
+
+    // state.cart.push(newCartInfo)
+    // console.log(state.cart)
+
+    // state.items.forEach((item) => {
+      // const cartItem = document.renderElement('li')
+      // console.log(e.target)
+      // if ()
+      // cartItem.innerText = item.name
+      // console.log(item.name)
+
+      // cartList.append(cartItem)
+
+    // })
+  })
+
+  return storeItemButton
 }
 
 // DISPLAY EACH OF THE STORE ITEMS ON THE PAGE
@@ -90,14 +111,61 @@ const createStoreItemDiv = (item) => {
 const renderStore = () => {
   state.items.forEach((item) => {
     const storeItem = document.createElement('li')
-    storeItem.innerText = "placeholder"
   
-    storeItem.append(createStoreItemDiv(item))
-    // storeItem.append(selectStoreItemImage(item))
-  
-    storeItem.append(createStoreItemButton())
+    storeItem.append(renderStoreItemDiv(item))  
+    storeItem.append(renderStoreItemButton())
     
     storeList.append(storeItem)
+  })
+}
+
+// CART
+// CREATE CART FUNCTIONS
+const renderCartItemImage = (item) => {
+  const cartItemImage = document.createElement('img')
+  cartItemImage.setAttribute('class', 'cart--item-icon')
+  cartItemImage.src = `assets/icons/${item.id}.svg`
+  cartItemImage.alt = item.name
+  return cartItemImage
+}
+
+const renderCartItemName = (item) => {
+  const cartItemName = document.createElement('p')
+  cartItemName.innerText = item.name
+  return cartItemName
+}
+
+const renderDecreaseButton = () => {
+  const cartItemDecreaseButton = document.createElement('button')
+  cartItemDecreaseButton.classList.add('quantity-btn', 'remove-btn', 'center')
+  cartItemDecreaseButton.innerText = '-'
+  return cartItemDecreaseButton
+}
+
+const renderCounter = () => {
+  const cartItemspan = document.createElement('span')
+  cartItemspan.classList.add('quantity-text', 'center')
+  return cartItemspan
+}
+
+const renderIncreaseButton = () => {
+  const cartItemIncreaseButton = document.createElement('button')
+  cartItemIncreaseButton.classList.add('quantity-btn', 'add-btn', 'center')
+  cartItemIncreaseButton.innerText = '+'
+  return cartItemIncreaseButton
+}
+
+const renderCart = () => {
+  state.items.forEach((item) => {
+    const cartItem = document.createElement('li')
+    
+    cartItem.append(renderCartItemImage(item))
+    cartItem.append(renderCartItemName(item))
+    cartItem.append(renderDecreaseButton())
+    cartItem.append(renderCounter())
+    cartItem.append(renderIncreaseButton())
+
+    cartList.append(cartItem)
   })
 }
 
