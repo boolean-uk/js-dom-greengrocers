e87fbc51c7514aa03fd01044cc68a36f792b4ca2
Chloe Zermatten <chloezermatten@gmail.com>
2023-10-16 10:38:22 +0100

    quantity counter functionality added

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-let counter = 0
+let counter = 1
 
 
 const increase = () => ++counter
@@ -8,25 +8,40 @@ const decrease = () => --counter
 const addIncreaseButton = () => {
     const increaseButton = document.createElement("button")
     increaseButton.innerText = '+'
-    increaseButton.addEventListener('click', e => increase())
+    increaseButton.setAttribute('class', 'quantity-btn add-btn center')
+    increaseButton.addEventListener('click', e => {
+        const initialCounter = cart.querySelector('span[class="quantity-text center"]')
+        initialCounter.remove()
+        increase()
+        addCounterDisplay()
+    })
     cartItem.append(increaseButton)
 }
 
 const addDecreaseButton = () => {
     const decreaseButton = document.createElement("button")
     decreaseButton.innerText = '-'  
-    decreaseButton.addEventListener('click', e => decrease())
+    decreaseButton.setAttribute('class', 'quantity-btn remove-btn center') 
+    decreaseButton.addEventListener('click', e => {
+        const initialCounter = cart.querySelector('span[class="quantity-text center"]')
+        initialCounter.remove()
+        decrease()
+        addCounterDisplay()
+    })
     cartItem.append(decreaseButton)
 }
 
 const addCounterDisplay = () => {
     const counterDisplay = document.createElement('span')
+    counterDisplay.setAttribute('class', 'quantity-text center')
     counterDisplay.innerText = counter
     cartItem.append(counterDisplay)
 }
 
 const addQuantityCounter = () => {
-    addIncreaseButton()
+   
+    addDecreaseButton() 
     addCounterDisplay()
-    addDecreaseButton()   
+    addIncreaseButton()
+      
 }
