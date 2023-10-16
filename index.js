26f2a3b33aa4552dbdac4e553a008ec52364fdd9
ilham saleh <ilham2saleh@gmail.com>
2023-10-16 11:44:06 +0100

    I've struggled a lot but finally and barely I did it

--- index.js
+++ index.js
@@ -54,9 +54,15 @@ const state = {
   cart: []
 };
 
+
+
+// -----------------------------------------------------------------------------------------------------------------
+
 const store = document.querySelector('#store')
-const storeUl = document.querySelector(".store--item-list");
-const cartUl = document.querySelector(".cart--item-list");
+const storeUl = store.querySelector(".store--item-list");
+const mainCart = document.querySelector("#cart");
+const cartUl = mainCart.querySelector(".cart--item-list")
+const totalSection = document.querySelector(".total-section");
 const price = document.querySelector(".total-number");
 
 const items = state.items;
@@ -83,18 +89,32 @@ function renderStoreList() {
     storeUl.append(storeList)
 
     storeButton.addEventListener('click', () => {
-      item.quantity =+ 1
-      cart.push(item)
+      const cartItemMatch = state.cart.find(eachCartItem => eachCartItem.id === item.id)
 
-      const cartList = document.createElement('li')
+      if (cartItemMatch) {
+        ++cartItemMatch.quantity
+      }else {
+        cart.push({...item, quantity: 1})
+      }
+      renderCartItems()
+    })
 
+  })
+}
+
+function renderCartItems(){
+  const cartLists = cartUl.querySelectorAll('li')
+  cartLists.forEach(cartList => cartList.remove())
+
+  cart.forEach(item => {
+    const cartList = document.createElement('li')
       const cartListImg = document.createElement('img')
       cartListImg.src = `assets/icons/${item.id}.svg`
       cartListImg.alt = `${item.name}`
-
+  
       const p = document.createElement('p')
       p.innerText = item.name
-
+  
       const removeButton = document.createElement("button");
       removeButton.className = "quantity-btn remove-btn center"
       removeButton.innerText = "-"
@@ -105,87 +125,37 @@ function renderStoreList() {
       addButton.className = "quantity-btn add-btn center"
       addButton.innerText = "+"
 
+      addButton.addEventListener('click', () => {
+        const cartItem = state.cart.find(cartItem => cartItem.id === item.id)
+        ++cartItem.quantity
+        renderCartItems()
+      })
+
+      removeButton.addEventListener('click', () => {
+        const cartItem = cart.find(cartItem => cartItem.id === item.id)
+        if (cartItem.quantity > 1) {
+          --item.quantity
+        } else {
+          // state.cart = state.cart.filter(cartItem => cartItem.id !== item.id)
+          state.cart.splice(cart.item, 1)
+          price.innerText = '£0.00'
+        }
+        renderCartItems()
+      })
+
+      const prices = state.cart.map((item) => item.price * item.quantity);
+      const total = prices.reduce((sum, item) => sum + item);
+      price.innerText = `£${total.toFixed(2)}`;
+
       cartList.append(cartListImg, p, removeButton, span, addButton)
       cartUl.append(cartList)
-    })
-
   })
 }
 
-function removeCartList() {
-  const cartLists = cartUl.querySelectorAll('li')
-  cartLists.forEach((cartList) => cartList.remove())
+function totalPrice() {
+  const prices = state.cart.map((item) => item.price * item.quantity);
+  const total = prices.reduce((sum, item) => sum + item);
+  price.innerText = `£${total.toFixed(2)}`;
 }
 
-
 renderStoreList()
-
-// function makeStoreList() {
-//   state.items.forEach((item) => {
-//     const storeList = document.createElement('li')
-//     storeList.innerText = item.name
-//     const storeUl = document.querySelector('.store--item-list')
-//     const addButton = document.createElement('button')
-//     addButton.innerText = 'Add item'
-//     const storeIconContainer = makeStoreIconContainer()
-//     const storeImage = makeBeetRoot()
-//     storeIconContainer.append(storeImage)
-//     storeList.append(storeIconContainer)
-//     storeList.append(addButton)
-
-//     addButton.addEventListener('click', (e) => {
-//       const cartItemList = document.querySelector('.cart--item-list')
-//       // const cartValue = e.target.value
-
-//       // cartItemList.append(cartValue)
-//       console.log(item.name)
-//     })
-    
-
-//     storeUl.append(storeList)
-
-//   });
-// }
-
-// makeStoreList()
-
-// function makeStoreIconContainer() {
-//   const storeIconContainer = document.createElement('div')
-//   storeIconContainer.className = "store--item-icon"
-
-//   return storeIconContainer
-
-// }
-// function makeBeetRoot(){
-//   const beetRoot = document.createElement('img')
-//   beetRoot.src = "assets/icons/001-beetroot.svg"
-//   beetRoot.alt = "beetroot"
-
-//   return beetRoot
-// }
-
-// function makeCarrot(){
-//   const carrot = document.createElement('img')
-//   beetRoot.src = "assets/icons/002-Carrot.svg"
-//   beetRoot.alt = "carrot"
-
-//   return carrot
-// }
-
-// // function renderStoreList() {
-// //   const storeUl = document.querySelector('.store--item-list')
-// //   const storeList = makeStoreList()
-// //   const storeIconContainer = makeStoreIconContainer()
-// //   const addButton = document.createElement('button')
-
-// //   const beetRoot = makeBeetRoot()
-// //   const carrot = makeCarrot()
-
-// //   storeIconContainer.append(beetRoot)
-// //   storeIconContainer.appen(carrot)
-
-// //   storeList.append(storeIconContainer)
-// //   storeUl.append(storeList)
-// // }
-
-// renderStoreList()
