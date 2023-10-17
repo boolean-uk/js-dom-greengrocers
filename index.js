bbf7d62b5f289b9805844d413eba3fde6c536491
aayushlama4008 <aayushlama4008@gmail.com>
2023-10-17 12:28:21 +0200

    render fucntion to cart done

--- index.js
+++ index.js
@@ -1,95 +1,153 @@
 const state = {
-  items: [
-    {
-      id: "001-beetroot",
-      name: "beetroot",
-      price: 0.35
-    },
-    {
-      id: "002-carrot",
-      name: "carrot",
-      price: 0.35
-    },
-    {
-      id: "003-apple",
-      name: "apple",
-      price: 0.35
-    },
-    {
-      id: "004-apricot",
-      name: "apricot",
-      price: 0.35
-    },
-    {
-      id: "005-avocado",
-      name: "avocado",
-      price: 0.35
-    },
-    {
-      id: "006-bananas",
-      name: "bananas",
-      price: 0.35
-    },
-    {
-      id: "007-bell-pepper",
-      name: "bell pepper",
-      price: 0.35
-    },
-    {
-      id: "008-berry",
-      name: "berry",
-      price: 0.35
-    },
-    {
-      id: "009-blueberry",
-      name: "blueberry",
-      price: 0.35
-    },
-    {
-      id: "010-eggplant",
-      name: "eggplant",
-      price: 0.35
-    }
-  ],
-  cart: []
-};
+    items: [
+      {
+        id: "001-beetroot",
+        name: "beetroot",
+        price: 0.35,
+      },
+      {
+        id: "002-carrot",
+        name: "carrot",
+        price: 0.35,
+      },
+      {
+        id: "003-apple",
+        name: "apple",
+        price: 0.35,
+      },
+      {
+        id: "004-apricot",
+        name: "apricot",
+        price: 0.35,
+      },
+      {
+        id: "005-avocado",
+        name: "avocado",
+        price: 0.35,
+      },
+      {
+        id: "006-bananas",
+        name: "bananas",
+        price: 0.35,
+      },
+      {
+        id: "007-bell-pepper",
+        name: "bell pepper",
+        price: 0.35,
+      },
+      {
+        id: "008-berry",
+        name: "berry",
+        price: 0.35,
+      },
+      {
+        id: "009-blueberry",
+        name: "blueberry",
+        price: 0.35,
+      },
+      {
+        id: "010-eggplant",
+        name: "eggplant",
+        price: 0.35,
+      },
+    ],
+    cart: [
+        {
+            id: "001-beetroot",
+            name: "beetroot",
+            price: 0.35,
+            quantity: 1
+          },
+          {
+            id: "002-carrot",
+            name: "carrot",
+            price: 0.35,
+            quantity: 1
+          },
+          {
+            id: "003-apple",
+            name: "apple",
+            price: 0.35,
+            quantity: 1
+          }
+    ],
+  };
 
+  //to render the items in the store
+  function renderStorelist(){
 
-//function to show the items in the store form the array
-const createStorelist =() => {
+    //to loop through everything in the state.items
+    state.items.forEach((item)=>{
+        
+        //select the ul from the header
+        const listOfItems = document.querySelector('.store--item-list')
+        
+        //select the li to create the image
+        const storeList = document.createElement('li')
+
+        //create a div for the store list
+        const itemDiv =document.createElement('div')
+        itemDiv.setAttribute('class','store--item-icon')
+
+        const itemImage = document.createElement('img')
+        itemImage.setAttribute('src',`assets/icons/${item.id}.svg`)
+        itemImage.setAttribute('alt','item.name')
+
+        const addButton = document.createElement('button')
+        addButton.innerText = 'Add to cart'
+
+        //add addeventlistener to the button
+        addButton.addEventListener('click',()=>{
+
+        })
+        //appends
+        itemDiv.append(itemImage)
+        storeList.append(itemDiv,addButton)
+        listOfItems.append(storeList)
+    })
+
+  }
+
+  function renderCartList() {
+    //select ul from cart
+      const cartList = document.querySelector('.cart--item-list');
   
-  //to loop through the items to display
-  state.items.forEach(product => {
+      //to remove items in the card after rendering
+      const removeItemsInTheCart =cartList.querySelectorAll('*');
+      removeItemsInTheCart.forEach((childElement)=>childElement.remove());
 
-     // to display the items in the store list 
-    const listOfItem = document.querySelector('.store--item-list')
-  
-    //create a li to store  the item in the store
-    const storeList = document.createElement('li')
+      //for eacg item in the cart
+    state.cart.forEach((cartItem) => {
     
-      const storeItemIcon = document.createElement('div')
-      storeItemIcon.setAttribute('class','store--item-icon')
-
-        //for the icon
-        const productIcon = document.createElement('img')
-        productIcon.setAttribute('src',`./assets/icons/${product.id}.svg`)
-    
-        storeItemIcon.append(productIcon)
-    
-       //for the button 
-       const button = document.createElement('button')
-       button.innerText = 'Add to card'
-
-       storeList.append(storeItemIcon)
-
-      storeList.append(button)
-
-
-      listOfItem.append(storeList)
-  })
-
-}
+        const cartListName = document.createElement('li');
   
-createStorelist();
+        const cartImage = document.createElement('img');
+        cartImage.src = `assets/icons/${cartItem.id}.svg`;
+        cartImage.className = 'cart--item-icon';
+        cartImage.alt = `${cartItem.name}`;
   
+        const cartName = document.createElement('p');
+        cartName.innerText = cartItem.name;
+  
+        const buttonMinus = document.createElement('button');
+        buttonMinus.setAttribute('class', 'quantity-btn remove-btn center');
+        buttonMinus.innerText = '-';
+    
+        const cartQuantity = document.createElement('span');
+         cartQuantity.setAttribute('class', 'quantity-text center');
+         cartQuantity.innerText = 1;
 
+        const buttonPlus = document.createElement('button');
+         buttonPlus .setAttribute('class', 'quantity-btn add-btn center');
+         buttonPlus .innerText = '+';
+         buttonPlus .value = cartItem.id
+    
+
+        cartListName.append(cartImage,cartName,cartQuantity,buttonMinus,buttonPlus);
+        cartList.append(cartListName);
+    })
+  
+  
+  }
+  renderStorelist();
+  renderCartList()
