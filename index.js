edddc020b2e0e2c7b6b845f21de95d56230ae9a1
Elizabeth <lizbeepmc@gmail.com>
2023-10-19 16:11:59 +0300

    done

--- index.js
+++ index.js
@@ -3,134 +3,177 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35
+      price: 0.35,
+      quantity: 0,
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35
-    }
+      price: 0.35,
+      quantity: 0,
+    },
   ],
-  cart: []
+  cart: [],
 };
-const renderHeader = () => {
 
-  state.items.forEach((item)=> {
-    const itemListContainer = document.querySelector('.item-list');
-    
-    
-    const listItem = document.createElement('li');
-    itemListContainer.append(listItem);
+// SELECT EXISTING HTML ELEMENTS
+const storeLists = document.querySelector(".store--item-list");
+const cart = document.querySelector(".cart--item-list");
+const totalNumber = document.querySelector(".total-number");
 
-    const imageBox = document.createElement('div');
-    imageBox.classList.add('store--item-icon')
-    listItem.append(imageBox);
+// RENDER FOR STORE ITEMS
+function renderHeader() {
+  storeLists.innerHTML = "";
 
-    const image= document.createElement('img');
-    image.src = `assets/icons/${item.id}.svg`
-    image.alt= item.name;
-    imageBox.append(image); 
-
-    const button= document.createElement('button');
-    button.innerText = 'add to cart';
-    listItem.append(button);
-    const eventButton = document.querySelector('button');
-    const newItem= document.querySelector('span');
-    eventButton.addEventListener ('click',() =>{
-      newItem ++;
-    })
-
-    });
-  
-}
-const renderMain = () => {
   state.items.forEach((item) => {
-    const itemListBox = document.querySelector('.cart--item-list');
+    const listItem = document.createElement("li");
+    storeLists.append(listItem);
 
-    const listItem2 = document.createElement('li');
-    itemListBox.append(listItem2);
+    const div = document.createElement("div");
+    div.setAttribute("class", "store--item-icon");
+    listItem.append(div);
 
-    const image= document.createElement('img');
-    image.src = `assets/icons/${item.id}.svg`
-    image.alt= item.name;
-    listItem2.append(image);
+    const img = document.createElement("img");
+    img.src = `assets/icons/${item.id}.svg`;
+    img.alt = item.id; // Use item.id instead of "${items.id}"
+    div.append(img);
 
-    const paragraph = document.createElement('p')
-    paragraph.innerText = item.name;
-    listItem2.append(paragraph);
+    const button = document.createElement("button");
+    button.innerText = "Add to cart";
+    listItem.append(button);
 
-    const addButton = document.createElement('button');
-    addButton.innerText = `+`;
-    listItem2.append(addButton);
-
-    addButton.addEventListener('click', () => {
-      const totalNumber = listItem2.querySelector('span');
-      let count = parseInt(totalNumber.innerText);
-      count++; // Increase the count.
-      totalNumber.innerText = count;
+    button.addEventListener("click", (event) => {
+      addItem(item);
+      renderMain();
+      cartTotal();
     });
-    
-    
-    const totalNumber = document.createElement('span');
-    totalNumber.innerText = '1';
-    listItem2.append(totalNumber);
-    
-    const removeButton= document.createElement('button');
-    removeButton.innerText = '-';
-    listItem2.append(removeButton);
-    removeButton.addEventListener('click', () => {
-      const totalNumber = listItem2.querySelector('span');
-      let count = parseInt(totalNumber.innerText);
-      if (count > 0) {
-        count--; // Decrease the count, but ensure it doesn't go below zero.
-        totalNumber.innerText =count;
-    }
-    });
-
-
-
-
-  })
+  });
 }
+//basket
+function addItem(item) {
+  if (state.cart.find((product) => product.name === item.name) === undefined) {
+    item.quantity = 1;
+    state.cart.push(item);
+    renderMain();
+  } else {
+    item.quantity++;
+    renderMain();
+  }
+}
+
+// RENDER FOR CART
+function renderMain() {
+  cart.innerHTML = " ";
+
+  state.cart.forEach((item) => {
+    const cartInLi = document.createElement("li");
+    cart.append(cartInLi);
+
+    const cartImage = document.createElement("img");
+    cartImage.setAttribute("class", "cart--item-icon");
+    cartImage.src = `assets/icons/${item.id}.svg`;
+    cartImage.alt = item.id; // Use item.id instead of "${items.id}"
+    cartInLi.append(cartImage);
+
+    const paragraph = document.createElement("p");
+    paragraph.innerText = `${item.name}`;
+    cartInLi.append(paragraph);
+
+const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "quantity-btn remove-btn center");
+    removeButton.innerText = "-";
+    cartInLi.append(removeButton);
+
+    removeButton.addEventListener("click", (event) => {
+      item.quantity -= 1;
+
+      if (item.quantity === 0) {
+        const index = state.cart.indexOf(item);
+        state.cart.splice(index, 1);
+      }
+      renderMain();
+      cartTotal();
+    });
+
+    const span = document.createElement("span");
+    span.setAttribute("class", "quantity-text center");
+    span.innerText = `${item.quantity}`;
+    cartInLi.append(span);
+
+    const addButton = document.createElement("button");
+    addButton.setAttribute("class", "quantity-btn add-btn center");
+    addButton.innerText = "+";
+    cartInLi.append(addButton);
+
+    addButton.addEventListener("click", (event) => {
+      item.quantity++;
+      renderMain();
+      cartTotal();
+    });
+  });
+}
+// total
+function cartTotal() {
+  let total = 0;
+  state.cart.forEach((item) => {
+    const price = item.price;
+    const quantity = item.quantity;
+    const itemTotal = price * quantity;
+    total += itemTotal;
+  });
+  let totalPrice = total.toFixed(2);
+  totalNumber.innerText = `£${totalPrice}`; // Use totalPrice instead of total
+}
+
+// 3. call renderHeader() as soon as the page loads
 renderHeader();
+cartTotal();
 renderMain();
