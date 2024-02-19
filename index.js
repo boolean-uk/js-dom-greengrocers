5bbf7ab0adcb4fa2f257714c5c9cbc61e582448e
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-19 11:21:35 +0100

    Finished core

--- index.js
+++ index.js
@@ -53,3 +53,114 @@ const state = {
   ],
   cart: []
 };
+
+const storeUl = document.querySelector('.store--item-list')
+const cartUl = document.querySelector('.cart--item-list')
+const total = document.querySelector('.total-number')
+
+
+function AddItem(item)
+{
+  console.log(`Add ${item.name} to card`)
+  //Check if already in cart
+  const inCart = state.cart.find(x => x.id === item.id)
+  //If it is, increase quantity
+  if (typeof inCart === 'undefined')
+  {
+    const newItem = {
+      id : item.id,
+      name : item.name,
+      price: item.price,
+      quantity:1
+    }
+    state.cart.push(newItem)
+  }
+  else {
+    inCart.quantity++
+  }
+  RenderAllCartItems()
+}
+
+function RemoveItem(item)
+{
+  item.quantity--
+  if (item.quantity === 0)
+  {
+    state.cart.splice(state.cart.indexOf(item), 1)
+  }
+  RenderAllCartItems()
+}
+
+function RenderAllCartItems()
+{
+  cartUl.innerHTML = ""
+  state.cart.forEach((item) => RenderCartItem(item))
+  total.innerHTML = `£${CalculatePrice()}`
+}
+
+function CalculatePrice()
+{
+  var result = 0
+  state.cart.forEach((x) => result += (x.price * x.quantity))
+  return result.toFixed(2)
+}
+
+
+
+function RenderCartItem(item)
+{
+  var li = document.createElement('li')
+  img = document.createElement('img')
+  img.src =`assets/icons/${item.id}.svg`
+  img.class = "cart--item-icon"
+  img.alt = `${item.name}`
+  var p = document.createElement('p')
+  p.appendChild(document.createTextNode(`${item.name}`))
+
+  var removeButton = document.createElement('button')
+  removeButton.class = "quantity-btn remove-btn center"
+  removeButton.addEventListener('click', () => RemoveItem(item))
+  removeButton.appendChild(document.createTextNode('-'))
+
+  var span = document.createElement('span')
+  span.class = "quantity-text center"
+  span.appendChild(document.createTextNode(`${item.quantity}`))
+  
+  var addButton = document.createElement('button')
+  addButton.class = "quantity-btn add-btn center"
+  addButton.addEventListener('click', ()=> AddItem(item))
+  addButton.appendChild(document.createTextNode('+'))
+   
+  li.appendChild(img)
+  li.appendChild(p)
+  li.appendChild(removeButton)
+  li.appendChild(span)
+  li.appendChild(addButton)
+  cartUl.appendChild(li)
+}
+
+function RenderStoreItem(item, ul)
+{
+  var li = document.createElement('li')
+  var cartDiv = document.createElement('div')
+  img = document.createElement('img')
+  img.src = `assets/icons/${item.id}.svg`
+  img.alt = "beetroot"
+  button = document.createElement('button');
+  button.addEventListener("click", () => AddItem(item))
+  button.appendChild(document.createTextNode('Add to cart'))
+  cartDiv.appendChild(img)
+  li.append(cartDiv)
+  li.appendChild(button)
+  ul.appendChild(li)
+
+}
+
+function Initialise()
+{
+  state.items.forEach((item) => RenderStoreItem(item, storeUl))
+  console.log("Initialise")
+}
+
+Initialise()
+
