7441ee121f31b3dee27a129d6596a55cb76ba6ca
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-19 13:10:48 +0100

    Finished extension 1: filter

--- index.js
+++ index.js
@@ -3,65 +3,75 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
+      type: "vegetable",
       price: 0.35
     },
     {
       id: "002-carrot",
       name: "carrot",
+      type: "vegetable",
       price: 0.35
     },
     {
       id: "003-apple",
       name: "apple",
+      type : "fruit",
       price: 0.35
     },
     {
       id: "004-apricot",
       name: "apricot",
+      type : "fruit",
       price: 0.35
     },
     {
       id: "005-avocado",
       name: "avocado",
+      type: "vegetable",
       price: 0.35
     },
     {
       id: "006-bananas",
       name: "bananas",
+      type : "fruit",
       price: 0.35
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
+      type: "vegetable",
       price: 0.35
     },
     {
       id: "008-berry",
       name: "berry",
+      type : "fruit",
       price: 0.35
     },
     {
       id: "009-blueberry",
       name: "blueberry",
+      type : "fruit",
       price: 0.35
     },
     {
       id: "010-eggplant",
       name: "eggplant",
+      type: "vegetable",
       price: 0.35
     }
   ],
   cart: []
 };
 
+const store = document.querySelector('#store')
 const storeUl = document.querySelector('.store--item-list')
 const cartUl = document.querySelector('.cart--item-list')
 const total = document.querySelector('.total-number')
-
+let filter = 'all'
 
 function AddItem(item)
 {
-  console.log(`Add ${item.name} to card`)
   //Check if already in cart
   const inCart = state.cart.find(x => x.id === item.id)
   //If it is, increase quantity
@@ -105,8 +115,6 @@ function CalculatePrice()
   return result.toFixed(2)
 }
 
-
-
 function RenderCartItem(item)
 {
   var li = document.createElement('li')
@@ -141,24 +149,64 @@ function RenderCartItem(item)
 
 function RenderStoreItem(item, ul)
 {
-  var li = document.createElement('li')
-  var cartDiv = document.createElement('div')
-  img = document.createElement('img')
-  img.src = `assets/icons/${item.id}.svg`
-  img.alt = "beetroot"
-  button = document.createElement('button');
-  button.addEventListener("click", () => AddItem(item))
-  button.appendChild(document.createTextNode('Add to cart'))
-  cartDiv.appendChild(img)
-  li.append(cartDiv)
-  li.appendChild(button)
-  ul.appendChild(li)
+  if (item.type === filter || filter === 'all')
+  {
+    var li = document.createElement('li')
+    var cartDiv = document.createElement('div')
+    img = document.createElement('img')
+    img.src = `assets/icons/${item.id}.svg`
+    img.alt = "beetroot"
+    button = document.createElement('button');
+    button.addEventListener("click", () => AddItem(item))
+    button.appendChild(document.createTextNode('Add to cart'))
+    cartDiv.appendChild(img)
+    li.append(cartDiv)
+    li.appendChild(button)
+    ul.appendChild(li)
+  }
 
+
+}
+
+
+function ApplyFilter(newFilter)
+{
+  console.log("Apply filter", newFilter)
+  filter = newFilter
+  RenderStore()
+}
+
+function CreateFilterButton(f, text)
+{
+  showAllButton = document.createElement('button')
+  showAllButton.appendChild(document.createTextNode(`${text}`))
+  showAllButton.addEventListener("click", () =>  ApplyFilter(`${f}`))
+  if (filter === `${f}`) showAllButton.setAttribute('disabled', 'disabled')
+
+  store.appendChild(showAllButton)
+}
+
+function RenderStore()
+{
+  //First clear everything
+   store.innerHTML = ""
+   storeUl.innerHTML = ""
+
+   h1 = document.createElement('h1')
+   h1.appendChild(document.createTextNode('Greengrocers'))
+
+   store.appendChild(h1)
+   CreateFilterButton('all', 'Show All')
+   CreateFilterButton('fruit', 'Fruit')
+   CreateFilterButton('vegetable', 'Vegetable')
+
+   store.appendChild(storeUl)
+   state.items.forEach((item) => RenderStoreItem(item, storeUl))
 }
 
 function Initialise()
 {
-  state.items.forEach((item) => RenderStoreItem(item, storeUl))
+  RenderStore()
   console.log("Initialise")
 }
 
