3274f27b63a107a5a254100eb81d6ec70fc7d2f0
Linus Willmont <linus.willmont@hotmail.com>
2024-02-19 19:58:43 +0100

    extensions

--- index.js
+++ index.js
@@ -7,51 +7,61 @@ const state = {
       id: "001-beetroot",
       name: "beetroot",
       price: 0.35,
+      colour: "pink",
     },
     {
       id: "002-carrot",
       name: "carrot",
       price: 0.35,
+      colour: "orange",
     },
     {
       id: "003-apple",
       name: "apple",
       price: 0.35,
+      colour: "red",
     },
     {
       id: "004-apricot",
       name: "apricot",
       price: 0.35,
+      colour: "orange",
     },
     {
       id: "005-avocado",
       name: "avocado",
       price: 0.35,
+      colour: "green",
     },
     {
       id: "006-bananas",
       name: "bananas",
       price: 0.35,
+      colour: "yellow",
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
       price: 0.35,
+      colour: "green",
     },
     {
       id: "008-berry",
       name: "berry",
       price: 0.35,
+      colour: "red",
     },
     {
       id: "009-blueberry",
       name: "blueberry",
       price: 0.35,
+      colour: "blue",
     },
     {
       id: "010-eggplant",
       name: "eggplant",
       price: 0.35,
+      colour: "purple",
     },
   ],
   cart: [],
@@ -68,6 +78,19 @@ function addToCart(item) {
   }
   renderBasketItems();
 }
+
+// TODO implement filter flags instead
+/*
+function addFilters() {
+  state.filters = [];
+  state.items.forEach((item) => {
+    if (!state.filters.includes(item.colour)) {
+      state.filters.push({ colour: item.colour, isActive: false });
+    }
+  });
+  console.log(state);
+}
+*/
 
 function removeFromCart(item) {
   const cartItem = state.cart.find((cartItem) => cartItem.id === item.id);
@@ -122,10 +145,22 @@ function createStoreItem(item) {
 }
 
 function renderStoreItems() {
+  clearStoreItems();
   const storeUL = document.querySelector(".item-list.store--item-list");
   state.items.forEach((item) => {
     const li = createStoreItem(item);
     storeUL.appendChild(li);
+  });
+}
+
+function renderStoreItemsWithFilter(filter) {
+  clearStoreItems();
+  const storeUL = document.querySelector(".item-list.store--item-list");
+  state.items.forEach((item) => {
+    if (item.colour === filter) {
+      const li = createStoreItem(item);
+      storeUL.appendChild(li);
+    }
   });
 }
 
@@ -175,8 +210,163 @@ function renderTotalCost() {
   totalSPAN.innerText = `£${calculateTotalCost().toFixed(2)}`;
 }
 
+function createClearFiltersLiButton() {
+  const clearFilterLi = document.createElement("li");
+  const clearButton = document.createElement("button");
+
+  clearFilterLi.classList.add("filter");
+
+  clearButton.textContent = "CLEAR FILTER";
+  clearButton.addEventListener("click", () => renderStoreItems());
+
+  clearFilterLi.appendChild(clearButton);
+  return clearFilterLi;
+}
+
+function createFilterLiButton(item) {
+  if (!document.getElementById(item.colour)) {
+    const filterLi = document.createElement("li");
+    const button = document.createElement("button");
+
+    filterLi.setAttribute("id", item.colour);
+    filterLi.classList.add("filter");
+
+    button.textContent = item.colour;
+    button.addEventListener("click", () =>
+      renderStoreItemsWithFilter(button.textContent)
+    );
+    filterLi.appendChild(button);
+    return filterLi;
+  } else {
+    return null;
+  }
+}
+
+function CreateFilterButtons(filterListUL) {
+  filterListUL.appendChild(createClearFiltersLiButton());
+  state.items.forEach((item) => {
+    const filterButton = createFilterLiButton(item);
+    if (filterButton != null) {
+      filterListUL.appendChild(filterButton);
+    }
+  });
+}
+
+function renderFilters() {
+  const filterListUL = document.querySelector(".filter-list");
+  CreateFilterButtons(filterListUL);
+}
+
+function createClearSortingsButton() {
+  const clearSortingsLi = document.createElement("li");
+  const clearButton = document.createElement("button");
+
+  clearSortingsLi.classList.add("sort");
+
+  clearButton.textContent = "CLEAR SORTING";
+  clearButton.addEventListener("click", () => {
+    let items = state.items;
+
+    items = items.sort(function (a, b) {
+      const itemA = a.id.toLowerCase();
+      const itemB = b.id.toLowerCase();
+      if (itemA < itemB) {
+        return -1;
+      }
+      if (itemA > itemB) {
+        return 1;
+      }
+      return 0;
+    });
+
+    state.items = items;
+
+    renderStoreItems();
+  });
+
+  clearSortingsLi.appendChild(clearButton);
+  return clearSortingsLi;
+}
+
+function createSortAZButton() {
+  const sortingLi = document.createElement("li");
+
+  const button = document.createElement("button");
+
+  sortingLi.classList.add("sort");
+
+  button.textContent = "A-Z";
+  sortingLi.appendChild(button);
+
+  button.addEventListener("click", () => {
+    let items = state.items;
+
+    items = items.sort(function (a, b) {
+      const itemA = a.name.toLowerCase();
+      const itemB = b.name.toLowerCase();
+      if (itemA < itemB) {
+        return -1;
+      }
+      if (itemA > itemB) {
+        return 1;
+      }
+      return 0;
+    });
+
+    state.items = items;
+
+    renderStoreItems();
+  });
+
+  return sortingLi;
+}
+
+function createSortZAButton() {
+  const sortingLi = document.createElement("li");
+
+  const button = document.createElement("button");
+
+  sortingLi.classList.add("sort");
+
+  button.textContent = "Z-A";
+  sortingLi.appendChild(button);
+
+  button.addEventListener("click", () => {
+    let items = state.items;
+
+    items = items.sort(function (a, b) {
+      const itemA = a.name.toLowerCase();
+      const itemB = b.name.toLowerCase();
+      if (itemA > itemB) {
+        return -1;
+      }
+      if (itemA < itemB) {
+        return 1;
+      }
+      return 0;
+    });
+
+    state.items = items;
+
+    renderStoreItems();
+  });
+
+  return sortingLi;
+}
+
+function renderSortings() {
+  const sortListUL = document.querySelector(".sort-list");
+  sortListUL.appendChild(createClearSortingsButton());
+  sortListUL.appendChild(createSortAZButton());
+  sortListUL.appendChild(createSortZAButton());
+}
+
 function clearBasketItems() {
   document.querySelector(".item-list.cart--item-list").innerHTML = "";
+}
+
+function clearStoreItems() {
+  document.querySelector(".item-list.store--item-list").innerHTML = "";
 }
 
 // INITIALISATION LOGIC
@@ -187,6 +377,9 @@ function main() {
 
   // perfrom initial render
   renderStoreItems(state.items);
+  renderFilters();
+  renderSortings();
+  // addFilters();
 
   // setup event handlers
 
