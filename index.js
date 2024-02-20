eef447f1c46f3ccdb735ecec26bbf30ee102e1d7
skjalg <spectraldesign1@gmail.com>
2024-02-20 15:43:40 +0100

    Core

--- index.js
+++ index.js
@@ -53,3 +53,94 @@ const state = {
   ],
   cart: []
 };
+
+function populateStore(){
+  const storeItemList = document.querySelector(".store--item-list");
+  storeItemList.innerHTML = "";
+  state.items.forEach((item) => {
+    const storeItem = document.createElement("li");
+    storeItem.innerHTML = `
+      <div class="store--item-icon">
+        <img src="assets/icons/${item.id}.svg" alt="${item.name}" />
+      </div>
+      <button>Add to cart</button>
+    `;
+    storeItem
+      .querySelector("button")
+      .addEventListener("click", () => {
+        addToCart(item);
+      });
+    storeItemList.appendChild(storeItem);
+  });
+}
+
+function addToCart(item){
+  if (state.cart.some(cartItem => cartItem.id === item.id)) {
+    changeCartItemQuantity(item, 1)
+    renderCart();
+    return;
+  }
+  else{
+    state.cart.push({
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      quantity: 1,
+    });
+    renderCart();
+  }
+}
+
+function renderCart(){
+  const cartItemList = document.querySelector(".cart--item-list");
+  cartItemList.innerHTML = "";
+  let total = 0;
+  state.cart.forEach((item) => {
+    cartItem = document.createElement("li");
+    cartItem.innerHTML = `
+      <img
+        class="cart--item-icon"
+        src="assets/icons/${item.id}.svg"
+        alt="${item.name}"
+      />
+      <p>${item.name}</p>
+      <button class="quantity-btn remove-btn center">-</button>
+      <span class="quantity-text center">${item.quantity}</span>
+      <button class="quantity-btn add-btn center">+</button>
+    `;
+    cartItemList.appendChild(cartItem);
+    total += item.price * item.quantity;
+
+    // Add event listeners for the quantity buttons
+    const quantityText = cartItem.querySelector(".quantity-text");
+    const quantityInt = parseInt(quantityText.textContent);
+    const removeButton = cartItem.querySelector(".remove-btn");
+    removeButton.addEventListener("click", () => {
+      if (quantityInt <= 1) {
+        state.cart = state.cart.filter(cartItem => cartItem.id !== item.id);
+      } 
+      else {
+        changeCartItemQuantity(item, -1);
+      }
+      renderCart();
+    });
+    const addButton = cartItem.querySelector(".add-btn");
+    addButton.addEventListener("click", () => {
+      changeCartItemQuantity(item, 1);
+      renderCart();
+    });
+  });
+  const totalNumber = document.querySelector(".total-number");
+  totalNumber.textContent = `£${total.toFixed(2)}`;
+}
+
+function changeCartItemQuantity(item, value){
+  state.cart = state.cart.map(cartItem => {
+    if (cartItem.id === item.id) {
+      return Object.assign({}, cartItem, { quantity: cartItem.quantity + value });
+    }
+    return cartItem;
+  });
+}
+
+populateStore();
