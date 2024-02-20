001fc664f721f38628d2276f0dfc1b5eb16ccb08
Victor <victor.adamson@se.experis.com>
2024-02-20 15:42:23 +0100

    Started from beginning, this is much better so far

--- index.js
+++ index.js
@@ -1,167 +1,169 @@
-//A user can view a selection of items in the store
-//From the store, a user can add an item to their cart
-//If the item is already in the cart, increase the item's quantity in the cart
-//From the cart, a user can view and adjust the number of items in their cart
-//If an item's quantity equals zero it is removed from the cart
-//A user can view the current total in their cart
-
-// SELECT EXISTING/FIXED DOM ELEMENTS
-const storeUl = document.querySelector(".store--item-list")
-const cartUl = document.querySelector(".cart--item-list")
-const totalElement = document.querySelector(".total-number")
-// DEFINE APPLICATION STATE (model)
+// Remember, this will make it easier to determine where the problem lies, logic or code
+// Plan each function with description, input, output, small steps in squence
+// Implement first step with code (3-5 lines)
+// Test it: comments, run, console.log
+// Implement next step
+// ...
 const state = {
   items: [
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35
-    }
+      price: 0.35,
+    },
   ],
-  cart: []
+  cart: [],
 };
 
+// SELECT EXISTING/FIXED DOM ELEMENTS
+const StoreUl = document.querySelector(".store--item-list");
+const CartUl = document.querySelector(".cart--item-list");
+
 // LOGIC (functions) TO UPDATE APP STATE (controller -> model -> view update)
-function handleIncrease(span, item) {
-  console.log('Increasing')
-  span.innerText++
-  //total++
+function addItemToCart(storeItem) {
+  // Description: recieves an item from state.items, 'turns' it into cartItem and renders it to the cart element. On "Add to cart"-button click
+  // Input: storeItem
+  // Output: NaN
+  console.log("Running addItemToCart");
+  // Convert storeItem into cartItem (add quantity)
+  let exists = false;
+  // Loop through state.cart to see if storeItem exists already
+  for (let i = 0; i < state.cart.length; i++) {
+    // if exists set "variable" to true
+    if (storeItem.id === state.cart[i].id) {
+      exists = true;
+      // Increment cartItem.quantity by 1
+      state.cart[i].quantity += 1;
+      break;
+    }
+  }
+  if (!exists) {
+    const cartItem = {
+      id: storeItem.id,
+      name: storeItem.name,
+      price: storeItem.price,
+      quantity: 1,
+    };
+    // Add cartItem to state.cart if not exists
+    state.cart.push(cartItem);
+  }
+  console.log(state.cart);
+  renderItemsInCart();
 }
-
-function handleDecrease(span, item) {
-  console.log(item.span)
-  span.innerText--
-  if (span.innerText >= 1) {
-    console.log('Decreasing')
-  }else {
-    console.log('Removed')
-    const element = document.getElementById(item.id)
-    element.remove()
-  }  
-  //total--
-}
-
 // LOGIC (functions) TO HANDLE USER EVENTS (view -> controller interaction)
-function handleAddItemToCart(item) {
-  console.log('show item: ', item)
-  const span = document.getElementsByTagName('span')
-  //const span = document.getElementById(item.id)
-  
-  // get span from this item
-  const cartli = renderCartData(item)
-  if (!!document.getElementById(item.id)) {
-    console.log('item already exists')
-    // Increase amount in cart
-    handleIncrease(span[0], item)
-    //handleIncrease(span, item)
-  }else {
-    cartUl.appendChild(cartli)
-  }  
-}
 
 // LOGIC (functions) TO HANDLE RENDERING / DISPLAY / CLEARING OF UI (view)
-function renderCartData(item) {
-  const li = document.createElement('li')
-  li.setAttribute('id', item.id)
+function renderItemsInCart() {
+  // Description: Render elements in state.cart to the cart HTML element
+  // Input: List of cartItems from state.cart
+  // Output: NaN
+  console.log("Rendering items to cart");
+  // Clear HTML
+  // Loop through cartItems in state.cart
+  CartUl.innerHTML = "";
+  for (let i = 0; i < state.cart.length; i++) {
+    // Create HTML Elements and set attributes
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.setAttribute("class", "cart--item-icon");
+    img.setAttribute("src", `assets/icons/${state.cart[i].id}.svg`);
+    const p = document.createElement("p");
+    p.innerText = state.cart[i].name;
 
-  const img = document.createElement('img')
-  img.setAttribute('class', "cart--item-icon")
-  img.setAttribute('src', `assets/icons/${item.id}.svg`, item.name)
-  li.appendChild(img)
+    const remBtn = document.createElement("button");
+    remBtn.setAttribute("class", "quantity-btn remove-btn center");
+    remBtn.innerText = "-";
+    //remBtn.addEventListener("click");
 
-  const p = document.createElement('p')
-  p.innerText = item.name
-  li.appendChild(p)
+    const span = document.createElement("span");
+    span.setAttribute("class", "quantity-text center");
+    span.innerText = state.cart[i].quantity;
 
-  const span = document.createElement('span')
-  span.setAttribute('class', "quantity-text center")
-  span.innerText = 1
+    const addBtn = document.createElement("button");
+    addBtn.setAttribute("class", "quantity-btn add-btn center");
+    addBtn.innerText = "+";
+    //addBtn.addEventListener("click");
 
-  const removeButton = document.createElement('button')
-  removeButton.setAttribute('class', "quantity-btn remove-btn center")
-  removeButton.innerText = '-'
-  removeButton.addEventListener('click', () => handleDecrease(span, item))
-  
-  const addButton = document.createElement('button')
-  addButton.setAttribute('class', "quantity-btn add-btn center")
-  addButton.innerText = '+'
-  addButton.addEventListener('click', () => handleIncrease(span, item))
+    // Append Elements to CartUl list
+    li.appendChild(img);
+    li.appendChild(p);
+    li.appendChild(remBtn);
+    li.appendChild(span);
+    li.appendChild(addBtn);
 
-  li.appendChild(removeButton)
-  li.appendChild(span)
-  li.appendChild(addButton)
-
-  return li
+    CartUl.appendChild(li);
+  }
 }
+function renderStoreItems() {
+  // Description: Render elements in state.items to HTML element
+  // Input: List of storeItems from state.items
+  // Output: NaN
+  console.log("Rendering items to the store");
+  // Loop through storeItems
+  for (let i = 0; i < state.items.length; i++) {
+    // Create HTML Elements and set attributes
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.setAttribute("class", "store--item-icon");
 
-function renderItemData(item) {
-  // Create list item
-  const li = document.createElement('li')
-  // Create div to keep item-icon in
-  const div = document.createElement('div')
-  div.setAttribute('class', "store--item-icon")
-  // Create and load item-icon
-  const img = document.createElement('img')
-  img.setAttribute('src', `assets/icons/${item.id}.svg`, `${item.name}`)
-  // Add img to div and add div to li
-  div.appendChild(img)
-  li.appendChild(div)
-  
-  // Create button for item
-  const button = document.createElement('button')
-  button.innerText = "Add to cart"
-  button.addEventListener('click', () => handleAddItemToCart(item))
-  
-  // Add button to li
-  li.appendChild(button)
+    const img = document.createElement("img");
+    img.setAttribute("src", `assets/icons/${state.items[i].id}.svg`);
 
-  // Return whole li
-  return li
+    const btn = document.createElement("button");
+    btn.innerText = "Add to cart";
+    btn.addEventListener("click", () => addItemToCart(state.items[i]));
+
+    // Append Elements to StoreUl list
+    li.appendChild(div);
+    li.appendChild(img);
+    li.appendChild(btn);
+    StoreUl.appendChild(li);
+  }
 }
 
 // INITIALISATION LOGIC
@@ -169,15 +171,10 @@ function initialise() {
   console.log("Initialising...");
 
   // perform any additional actions to load state
-  
-  // perfrom initial render
-  state.items.forEach((item) => {
-    // Call function to load this li
-    const li = renderItemData(item)
-    // Add li to the list
-    storeUl.appendChild(li)
-  })
 
+  // perfrom initial render
+  renderStoreItems();
+  renderItemsInCart();
   // setup event handlers
   console.log("Initialisation done.");
 }
