f5ae78285360e5a161cad885a70162b15c0d33d8
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-20 15:58:53 +0100

    Implement basic functionality

---
+++ storeItem.js
@@ -0,0 +1,35 @@
+import { renderCartItem } from "./cartItem.js";
+
+export function renderStoreItems(items, cart) {
+  const storeItemsList = document.getElementById("store-item-list");
+  for (let i = 0; i < items.length; i++) {
+    const storeItem = storeItemComponent(items[i], cart);
+    storeItemsList.appendChild(storeItem);
+  }
+}
+
+function storeItemComponent(item, cart) {
+  const container = document.createElement("li");
+
+  const storeItemIcon = document.createElement("div");
+  storeItemIcon.className = "store--item-icon";
+  const img = document.createElement("img");
+  img.src = `assets/icons/${item.id}.svg`;
+  img.alt = item.name;
+  storeItemIcon.appendChild(img);
+
+  const button = document.createElement("button");
+  button.innerText = "Add to cart";
+  button.onclick = () => addToCart(item, cart);
+
+  container.appendChild(storeItemIcon);
+  container.appendChild(button);
+
+  return container;
+}
+
+const addToCart = (item, cart) => {
+  console.log(cart);
+  cart.push(item);
+  renderCartItem(item);
+};
