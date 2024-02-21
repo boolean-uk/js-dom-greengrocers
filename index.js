96a7fb2fc7d029f7ff32fadd64a3d1603fd68338
Eddy1108 <edvardh@hotmail.no>
2024-02-21 15:48:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,3 +53,34 @@ const state = {
   ],
   cart: []
 };
+
+const createStore = (poke) => {
+
+  let itemName = poke.name
+  itemName = itemName.charAt(0).toUpperCase() + itemName.slice(1)
+  
+  return `<li class="card">
+      <h2 class="card--title">${pokeName}</h2>
+      <img
+        width="256"
+        class="card--img"
+        src=${poke.sprites.other["official-artwork"].front_default}
+      />
+      <ul class="card--text">
+        <li>HP: ${poke.stats[0].base_stat}</li>
+        <li>ATTACK: ${poke.stats[1].base_stat}</li>
+        <li>DEFENSE: ${poke.stats[2].base_stat}</li>
+        <li>SPECIAL-ATTACK: ${poke.stats[3].base_stat}</li>
+        <li>SPECIAL-DEFENSE: ${poke.stats[4].base_stat}</li>
+        <li>SPEED: ${poke.stats[5].base_stat}</li>
+      </ul>
+    </li>`
+    
+  }
+  
+  const createAllPokes = (pokelist) => {
+      return pokelist.map(p => createPoke(p))
+  }
+  
+  document.getElementsByClassName('cards')[0].innerHTML = createAllPokes(data).join('')
+  
