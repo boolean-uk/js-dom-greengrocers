de215553f74f4199dc32db7b8c2cf597ea3e1aad
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-21 10:54:27 +0100

    Completed core

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const storeUl = document.querySelector('.store--item-list');
+const cartUl = document.querySelector('.cart--item-list')
+const totalSpan = document.querySelector('.total-number')
+
 const state = {
   items: [
     {
@@ -53,3 +57,115 @@ const state = {
   ],
   cart: []
 };
+
+//Main
+renderStore()
+
+//Functions
+function renderStore(){
+  state.items.forEach(item => {
+    const product = document.createElement('li');       
+  
+    const image = document.createElement('img');
+    image.classList.add('store--item-icon');
+    image.src = `assets/icons/${item.id}.svg`;
+    product.appendChild(image);
+  
+    const addButton = document.createElement('button');
+    addButton.classList.add('button');
+    addButton.innerText = 'Add to Cart';
+    addButton.addEventListener('click', () => addToCart(item));
+
+    product.append(addButton);
+  
+    storeUl.append(product);
+  });
+}
+
+function addToCart(item) {
+  const existingItem = state.cart.find(element => element.id === item.id);
+
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    const newItem = convert(item);
+    state.cart.push(newItem);
+  }
+
+  renderCart();
+}
+
+
+function convert(shopItem){
+  return {
+    id: shopItem.id,
+    name: shopItem.name,
+    price: shopItem.price,
+    quantity: 1
+  }
+}
+
+function renderCart(){
+  cartUl.innerHTML = '';
+
+  state.cart.forEach(item => {
+    const product = document.createElement('li')
+
+    const image = document.createElement('img')
+    image.classList.add('cart--item-icon')
+    image.src = `assets/icons/${item.id}.svg`;
+    product.appendChild(image)
+
+    const name = document.createElement('p')
+    name.innerText = item.name
+    product.appendChild(name)
+
+    const removeButton = document.createElement('button')
+    removeButton.classList.add('remove-btn')
+    removeButton.innerText = '-'
+    removeButton.addEventListener('click', () => removeFromCart(item))
+    product.appendChild(removeButton)
+
+    const quantityText = document.createElement('p')
+    quantityText.classList.add('quantity-text')
+    quantityText.innerText = item.quantity
+    product.appendChild(quantityText)
+
+    const addButton = document.createElement('button')
+    addButton.classList.add('add-btn')
+    addButton.innerText = '+'
+    addButton.addEventListener('click', () => addToCart(item))
+    product.appendChild(addButton)
+
+    cartUl.appendChild(product)
+  })
+
+  updateTotal()
+}
+
+function removeFromCart(item) {
+  const existingItem = state.cart.find(element => element.id === item.id);
+
+  if (existingItem) {
+    existingItem.quantity--;
+
+    if (existingItem.quantity <= 0) {
+      const index = state.cart.indexOf(existingItem);
+      state.cart.splice(index, 1);
+    }
+  }
+
+  renderCart();
+}
+
+function updateTotal(){
+  let sum = 0
+
+  state.cart.forEach(element => {
+    sum += element.price * element.quantity
+  });
+
+  totalSpan.innerText =  "£"+sum.toFixed(2)
+}
+
+
