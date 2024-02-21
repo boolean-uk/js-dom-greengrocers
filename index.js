0d63508b83ed1e13883525ba6333d6e65951045b
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-21 14:52:31 +0100

    Completed Extended 2

--- index.js
+++ index.js
@@ -1,57 +1,75 @@
 const state = {
   items: [
-    {id: "001-beetroot", name: "beetroot", price: 0.35, type: "vegetable"},
-    {id: "002-carrot", name: "carrot", price: 0.35, type: "vegetable"},
+    {id: "001-beetroot", name: "beetroot", price: 0.75, type: "vegetable"},
+    {id: "002-carrot", name: "carrot", price: 0.30, type: "vegetable"},
     {id: "003-apple", name: "apple", price: 0.35, type: "fruit"},
     {id: "004-apricot", name: "apricot", price: 0.35, type: "fruit"},
-    {id: "005-avocado", name: "avocado", price: 0.35, type: "fruit"},
-    {id: "006-bananas", name: "bananas", price: 0.35, type: "fruit"},
-    {id: "007-bell-pepper", name: "bell pepper", price: 0.35, type: "vegetable"},
-    {id: "008-berry", name: "berry", price: 0.35, type: "fruit"},
-    {id: "009-blueberry", name: "blueberry", price: 0.35, type: "fruit"},
-    {id: "010-eggplant", name: "eggplant", price: 0.35, type: "vegetable"}
+    {id: "005-avocado", name: "avocado", price: 1.55, type: "fruit"},
+    {id: "006-bananas", name: "bananas", price: 0.65, type: "fruit"},
+    {id: "007-bell-pepper", name: "bell pepper", price: 0.45, type: "vegetable"},
+    {id: "008-berry", name: "berry", price: 0.15, type: "fruit"},
+    {id: "009-blueberry", name: "blueberry", price: 0.15, type: "fruit"},
+    {id: "010-eggplant", name: "eggplant", price: 1, type: "vegetable"}
   ],
   cart: []
 };
 
-let displayFruits = true
-let displayVegetables = true 
-
 const filters = {
   'Fruit': true,
-  'Vegetable': true
+  'Vegetable': true,
+}
+
+const sorting = {
+  'Price': null,
+  'Alphabetical': null
 }
 
 //Main
-renderFilterButtons()
+renderRibon()
 renderStore()
 
 
 //Functions
-function renderFilterButtons() {
-  const filterDiv = document.querySelector('.filters');
+function renderRibon() {
+  const ribbonDiv = document.querySelector('.ribbon');
 
   const fruitFilterButton = document.createElement('button');
   updateFilterButtonClass(filters.Fruit, fruitFilterButton);
-  fruitFilterButton.innerText = 'Fruits';
+  fruitFilterButton.innerText = 'Fruits  ';
   fruitFilterButton.addEventListener('click', () => {
     updateFilter('Fruit', fruitFilterButton);
   });
-  filterDiv.append(fruitFilterButton);
+  ribbonDiv.append(fruitFilterButton);
 
   const vegetableFilterButton = document.createElement('button');
   updateFilterButtonClass(filters.Vegetable, vegetableFilterButton);
-  vegetableFilterButton.innerText = 'Vegetables';
+  vegetableFilterButton.innerText = 'Vegetables  ';
   vegetableFilterButton.addEventListener('click', () => {
     updateFilter('Vegetable', vegetableFilterButton);
   });
-  filterDiv.append(vegetableFilterButton);
+  ribbonDiv.append(vegetableFilterButton);
+
+  const shortByPriceButton = document.createElement('button')
+  shortByPriceButton.classList.add('button')
+  shortByPriceButton.innerText = 'Sort by price  '
+  shortByPriceButton.addEventListener('click', () => {
+    updateShorting('Price', shortByPriceButton)
+  });
+  ribbonDiv.append(shortByPriceButton)
+
+  const sortByAlphabet = document.createElement('button')
+  sortByAlphabet.classList.add('button')
+  sortByAlphabet.innerText = 'Sort alphabetically  '
+  sortByAlphabet.addEventListener('click', () => {
+    updateShorting('Alphabetical', sortByAlphabet)
+  });
+  ribbonDiv.append(sortByAlphabet)
 }
 
-function updateFilter(filtername, button){
-  filters[filtername] = !filters[filtername]
+function updateFilter(name, button){
+  filters[name] = !filters[name]
 
-  updateFilterButtonClass(filters[filtername], button);
+  updateFilterButtonClass(filters[name], button);
 
   renderStore()
 }
@@ -65,6 +83,36 @@ function updateFilterButtonClass(display, button) {
   }
 }
 
+function updateShorting(name, button) {
+  if (sorting[name] === null)
+    sorting[name] = true
+  else
+    sorting[name] = !sorting[name] 
+
+  if (name == 'Price'){
+    if (sorting[name])
+      state.items.sort((a, b) => a[name.toLowerCase()] - b[name.toLowerCase()]);
+    else
+      state.items.sort((b, a) => a[name.toLowerCase()] - b[name.toLowerCase()]);
+  }
+  else if (name === 'Alphabetical'){
+    if (sorting[name])
+      state.items.sort((a, b) => a.name.localeCompare(b.name));
+    else
+    state.items.sort((b, a) => a.name.localeCompare(b.name));
+  }
+
+  updateFilterButtonText(sorting[name], button)
+
+  renderStore()
+}
+
+function updateFilterButtonText(shortDirection, button) {
+  if (shortDirection)
+    button.textContent = button.textContent.slice(0, -1) + '↑';
+  else
+    button.textContent = button.textContent.slice(0, -1) + '↓';
+}
 
 function renderStore() {
   const storeUl = document.querySelector('.store--item-list');
