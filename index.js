2fbda34ddaae671d24725f49802a4bd55e3fd5e7
Nicolai Klokmose <angryniczorn@gmail.com>
2024-02-22 10:55:40 +0100

    initial commit

--- index.js
+++ index.js
@@ -53,3 +53,61 @@ const state = {
   ],
   cart: []
 };
+
+
+
+// Select Root Elements
+const groceriesListUL = document.querySelector(".item-list.store--item-list");
+
+// Function to create an Item list item
+function createItemListItem(item) {
+  const itemLi = document.createElement('li');
+  itemLi.classList.add('store--item-icon');
+
+  // const itemName = document.createElement('h2');
+  // itemName.classList.add('item--title');
+  // itemName.innerText = item.name;
+  // itemLi.appendChild(itemName);
+
+  const itemImg = createItemImage(item.id);
+  itemLi.appendChild(itemImg);
+
+  const addToCartButton = createAddToCartButton(item);
+  itemLi.appendChild(addToCartButton);
+  return itemLi;
+}
+
+function createItemImage(itemId) {
+  const itemImg = document.createElement('img');
+  itemImg.classList.add('card--img');
+  itemImg.setAttribute('src', `assets/icons/${itemId}.svg`);
+  itemImg.setAttribute('width', '100');
+  itemImg.setAttribute('height', '100')
+  return itemImg;
+}
+
+function createAddToCartButton(item) {
+  const button = document.createElement('button');
+  button.innerText = 'ADD TO CART';
+  button.addEventListener('click', () => {
+    console.log(`${item.name} added`);
+  })
+  return button;
+}
+
+// Function to render the list of Groceries
+function renderGroceries() {
+  groceriesListUL.innerHTML = "";
+  state.items.forEach(item => {
+    const itemLi = createItemListItem(item);
+    groceriesListUL.appendChild(itemLi);
+  });
+}
+
+
+// Render
+function main () {
+  renderGroceries();
+}
+
+main();
