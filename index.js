353bab28684ef0b8e8b540893cd3f5dc977f983a
Gustav Svennas <gustavsvennas@gmail.com>
2024-02-22 16:19:44 +0100

    Almost done with core, need to find a way to update span

--- index.js
+++ index.js
@@ -53,3 +53,121 @@ const state = {
   ],
   cart: []
 };
+
+const cartItems = {};
+
+// Selected Root Element
+const storeItemListUL = document.querySelector(".store--item-list")
+const cartItemListUL = document.querySelector(".cart--item-list")
+
+function renderStoreItems() {
+  storeItemListUL.innerHTML = ""
+  for (let i = 0; i < state.items.length; i++) {
+    const item = state.items[i]
+
+    const itemLI = document.createElement('li')
+
+    const itemDIV = document.createElement('div')
+    itemDIV.setAttribute('class', 'store--item-icon')
+    createImageForStoreItem(itemDIV, item, "assets/icons/" + item.id + ".svg")
+
+    itemLI.appendChild(itemDIV)
+
+    createButtonForItem(itemLI, item)
+
+    storeItemListUL.appendChild(itemLI)
+  }
+}
+
+function createImageForStoreItem(itemDIV, item, imageSrc) {
+  const itemIMG = document.createElement('img')
+  itemIMG.src = imageSrc
+  itemIMG.alt = item.name
+  itemDIV.appendChild(itemIMG)
+}
+
+function createButtonForItem(itemLI, item) {
+  const itemButton = document.createElement('button')
+  itemButton.setAttribute('type', 'button')
+  itemButton.textContent = 'ADD TO CART'
+  itemButton.addEventListener('click', () => {addItemToCart(itemLI, item)})
+  itemLI.appendChild(itemButton)
+}
+
+function addItemToCart(itemLI, item) {
+  if (cartItems.hasOwnProperty(item.name)) {
+    cartItems[item.name] += 1
+  } else {
+    cartItems[item.name] = 1
+    createCartItem(item)
+  }
+  console.log(cartItems)
+}
+
+function createCartItem(item) {
+  const itemLI = document.createElement('li')
+
+  createImageForCartItem(itemLI, item, "assets/icons/" + item.id + ".svg")
+  
+  const itemP = document.createElement('p')
+  itemP.innerText = item.name
+  itemLI.appendChild(itemP)
+
+  createButtonsForCartItem(itemLI, item)
+
+  cartItemListUL.appendChild(itemLI)
+}
+
+function createImageForCartItem(itemLI, item, imageSrc) {
+  const itemIMG = document.createElement('img')
+  itemIMG.setAttribute('class', 'cart--item-icon')
+  itemIMG.src = imageSrc
+  itemIMG.alt = item.name
+  itemLI.appendChild(itemIMG)
+}
+
+function createButtonsForCartItem(itemLI, item) {
+  const quantityDisplayer = document.createElement('span')
+  quantityDisplayer.setAttribute('class', 'quantity-text center')
+  quantityDisplayer.textContent = cartItems[item.name]
+  quantityDisplayer.addEventListener('')
+
+  const buttonMinus = document.createElement('button')
+  //buttonMinus.setAttribute('type', 'button')
+  buttonMinus.setAttribute('class', 'quantity-btn remove-btn center')
+  buttonMinus.textContent = '-'
+  buttonMinus.addEventListener('click', () => {increaseItemAmount(item, quantityDisplayer)})
+  
+  const buttonPlus = document.createElement('button')
+  //buttonPlus.setAttribute('type', 'button')
+  buttonPlus.setAttribute('class', 'quantity-btn add-btn center')
+  buttonPlus.textContent = '+'
+  buttonPlus.addEventListener('click', () => {increaseItemAmount(item)})
+  
+  itemLI.appendChild(buttonMinus)
+  itemLI.appendChild(quantityDisplayer)
+  itemLI.appendChild(buttonPlus)
+}
+
+function increaseItemAmount(item, quantityDisplayer) {
+  quantityDisplayer.textContent = 5
+}
+
+function decreaseItemAmount(item) {
+
+}
+
+function createOrUpdateQuantityDisplay(itemLI, item, quantity) {
+  if (quantity === 1) {
+    const quantityDisplayer = document.createElement('span')
+    quantityDisplayer.setAttribute('class', 'quantity-text center')
+    quantityDisplayer.textContent = cartItems[item.name]
+    itemLI.appendChild(quantityDisplayer)
+  }
+}
+
+function main() {
+  renderStoreItems()
+}
+
+main()
