c9a0ea9a6bd885f56e524b23eaad1f9f88ffa88d
Farshad <f.bagdeli13@gmail.com>
2024-03-28 12:55:02 +0100

    added images to the GreenGrocers

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35
+      
+      price: 0.35,
     },
     {
       id: "002-carrot",
@@ -53,3 +54,45 @@ const state = {
   ],
   cart: []
 };
+
+console.log()
+const headerStore = document.querySelector('#store')
+const storeItemList = document.querySelector('.store--item-list')
+const mainCart = document.querySelector('#cart')
+const cartItemListContainer = document.querySelector('.cart--item-list-container')
+const cartItemList = document.querySelector('.cart--item-list')
+const totalSection = document.querySelector('.total-section')
+const totalNbr = document.querySelector('.total-number')
+
+// for ul card and ul store
+const itemList = document.querySelector('.item-list')
+
+function render() {
+
+  showStoreItems()
+
+
+  // storeItemList.append()
+}
+
+function showStoreItems(){
+  const items = state.items
+  for(element in items) {
+    const liItem = document.createElement('li')
+    liItem.style.listStyleType = 'none'
+    const divItem = document.createElement('div')
+    divItem.classList.add('store--item-icon')
+    const image = document.createElement('img')
+    image.setAttribute('src',`./assets/icons/${items[element].id}.svg`)
+
+    image.setAttribute('alt',`${items[element].name}`)
+    
+    const button = document.createElement('button')
+    button.innerText = 'Add to cart'
+
+    divItem.append(image)
+    liItem.append(divItem, button)
+    storeItemList.append(liItem)
+  };
+}
+render()
