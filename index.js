854c1540bb34bfc6c1a14e789663be9595aad5df
Farshad <f.bagdeli13@gmail.com>
2024-04-02 15:05:51 +0200

    Type Buttons Added

--- index.js
+++ index.js
@@ -3,52 +3,61 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      
+      type: "Vegetable",
       price: 0.35,
     },
     {
       id: "002-carrot",
       name: "carrot",
+      type: "Vegetable",
       price: 0.35
     },
     {
       id: "003-apple",
       name: "apple",
+      type : "Fruits",
       price: 0.35
     },
     {
       id: "004-apricot",
       name: "apricot",
+      type : "Fruits",
       price: 0.35
     },
     {
       id: "005-avocado",
       name: "avocado",
+      type: "Vegetable",
       price: 0.35
     },
     {
       id: "006-bananas",
       name: "bananas",
+      type : "Fruits",
       price: 0.35
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
+      type: "Vegetable",
       price: 0.35
     },
     {
       id: "008-berry",
       name: "berry",
+      type : "Fruits",
       price: 0.35
     },
     {
       id: "009-blueberry",
       name: "blueberry",
+      type : "Fruits",
       price: 0.35
     },
     {
       id: "010-eggplant",
       name: "eggplant",
+      type: "Vegetable",
       price: 0.35
     }
   ],
@@ -66,7 +75,9 @@ const totalNbr = document.querySelector('.total-number')
 // for ul card and ul store
 const itemList = document.querySelector('.item-list')
 
-const objectNo = { number : 0}
+const objectNo = {
+   number : 0
+  }
 tempItems = state.items
 
 for(i in tempItems){
@@ -75,14 +86,47 @@ for(i in tempItems){
 
 let itemsInCart = state.cart
 const itemsList = state.items
-function render() {
 
-  showStoreItems()
+function render() {
+  const typeGreenGrocers = 'Greengrocers'
   
+  seletType()
+  showStoreItems(typeGreenGrocers)
 }
 
-function showStoreItems(){
+function seletType(){
+  headerStore.innerHTML = ''
+  const h1Header = document.createElement('h1')
+  h1Header.innerText = 'Greengrocers'
+  const allBtn = document.createElement('Button')
+  allBtn.innerText = 'Greengrocers'
+  allBtn.addEventListener('click', () => {
+    h1Header.innerText = 'Greengrocers'
+    const typeSeleted = 'Greengrocers'
+    showStoreItems(typeSeleted)
+  })
+  const vegetableBtn = document.createElement('Button')
+    vegetableBtn.innerText = 'Vegetable'
+    vegetableBtn.addEventListener('click', () => {
+    const typeSeleted = 'Vegetable'
+    h1Header.innerText = 'Vegetable'
+    showStoreItems(typeSeleted)
+  })
+  const fruitBtn = document.createElement('Button')
+  fruitBtn.innerText = 'Fruits'
+  fruitBtn.addEventListener('click', () => {
+    const typeSeleted = 'Fruits'
+    h1Header.innerText = 'Fruits'
+    showStoreItems(typeSeleted)
+  })
+  headerStore.append(allBtn,vegetableBtn,fruitBtn, h1Header, storeItemList)
+}
+function showStoreItems(type){
+
+  storeItemList.innerHTML = ''
   for(element in itemsList) {
+    
+    if(itemsList[element].type === type) {
     const liItem = document.createElement('li')
     liItem.style.listStyleType = 'none'
     const divItem = document.createElement('div')
@@ -92,7 +136,19 @@ function showStoreItems(){
     divItem.append(image)
     liItem.append(divItem, button)
     storeItemList.append(liItem)
+    } else if(type === 'Greengrocers'){
+      const liItem = document.createElement('li')
+    liItem.style.listStyleType = 'none'
+    const divItem = document.createElement('div')
+    divItem.classList.add('store--item-icon')
+    const image = createStoreImage(itemsList)
+    const button = createStoreButton(itemsList, element)
+    divItem.append(image)
+    liItem.append(divItem, button)
+    storeItemList.append(liItem)
+    }
   };
+
 }
 
 function createStoreImage(items){
@@ -139,21 +195,15 @@ function showCartItems(items){
   for(element in items){
     if(items[element].number === 0 ){
       items.splice(element,1)
-      const pName = createCartPName (items[element]) 
-      const liItem = document.createElement('li')
-      const image = createCartImage(items[element])
-      const buttonMinus = createMinusButton(items,element)
-      const span = createSpan(items[element])
-      const buttonPlus = createPlusButton(items , element)
 
-      let totalValue = items[element].price * items[element].number
-      
-      totalNbr.innerText = `£ : ${totalValue.toFixed(2)}`
-      
-      liItem.append(image, pName, buttonMinus, span, buttonPlus)
-      cartItemList.append(liItem)
+      if(items.length === 0){
+      } else 
+      {
+        showCartItems(items)
+        
+      }
     } else {
-      const pName = createCartPName (items[element]) 
+      const pName = createCartPName(items[element]) 
       const liItem = document.createElement('li')
       const image = createCartImage(items[element])
       const buttonMinus = createMinusButton(items,element)
