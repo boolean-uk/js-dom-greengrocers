20b5651acc41d3a80381ea3d3a17df48a6680783
Tim Zoltie <t.zoltie@gmail.com>
2024-04-10 11:46:07 +0100

    each list item created and updating when added

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const pageHeader = document.querySelector('#store')
 const listOfItemsToBuy = document.querySelector('.item-list.store--item-list')
 const main = document.querySelector('#cart')
 const cartContainer = document.querySelector('.cart--item-list-container')
-const listItemsInCart = document.querySelector('.item-list.cart--item-list')
+// const listItemsInCart = document.querySelector('.item-list cart--item-list')
 const totalSection = document.querySelector('.total-section')
 const totalNumber = document.querySelector('.total-number')
 const cart = state.cart
@@ -78,23 +78,29 @@ function creteItemsInHeader() {
   listItem.append(itemIconContainer)
 
     
-  createItemImage(groceryItem)
   const imageOfItems = createItemImage(groceryItem)
   listItem.append(imageOfItems)
   
     
   const addToCartButton = document.createElement('button')
   addToCartButton.innerText = 'Add to cart'
+  // addToCartButton.addEventListener('click', () =>{addItemsToCart(groceryItem)})
+  addToCartButton.addEventListener('click', () =>{
+    const checkItemsInCart = cart.find((item) => item.id === groceryItem.id)
+  if (checkItemsInCart) {
+    checkItemsInCart.quantity++
+  } else {
+    const increaseQuantity = { ...groceryItem }
+    increaseQuantity.quantity = 1
+    cart.push(increaseQuantity)
+  }
+  renderCart()})
+
   listItem.append(addToCartButton)
-
-  addToCartButton.addEventListener('click', () =>{addItemsToCart(groceryItem)})
-
   }  
 }
 
 creteItemsInHeader()
-
-
 
 function createItemImage(groceryItem) {
   const itemImage = document.createElement('img')
@@ -103,111 +109,83 @@ function createItemImage(groceryItem) {
   return itemImage
 }
 
+function renderCart() {
+  const listItemsInCart = document.querySelector('.cart--item-list')
+  listItemsInCart.innerHTML =''
 
-function addItemsToCart(groceryItem) {
-  cart.push(groceryItem)
-  const groceryItemId = groceryItem.id
-  const checkItemsInCart = cart.find((element) => {
-    element.id === groceryItemId
-  })
-    if (checkItemsInCart) {
-      cart.push(groceryItem)
-      countItems(groceryItem)
+  for (let i = 0; i < cart.length; i++) {
+    const cartListItem = createCartItems(cart[i])
+    listItemsInCart.append(cartListItem)
+  }
+  cartTotal()
 }
-else {createCartItems(groceryItem)}
-}
-
 
 function createCartItems(groceryItem) {
-
-
     const listItem = document.createElement('li')
     
     createItemImage(groceryItem)
     const imageOfItems = createItemImage(groceryItem)
     listItem.append(imageOfItems)
   
-  
-  
     const itemName = document.createElement('p')
     itemName.innerText = groceryItem.name
     listItem.append(itemName)
   
-    
-  
     const removeBtn = document.createElement('button')
     removeBtn.classList = 'quantity-btn remove-btn center'
     removeBtn.innerText = '-'
+    removeBtn.addEventListener('click', () => {
+      if (groceryItem.quantity === 1) {
+      const itemIndex = cart.findIndex(item => item.id === groceryItem.id)
+      cart.splice(itemIndex, 1)
+      renderCart()
+      return
+    }
+    groceryItem.quantity--
+    renderCart()})
+
     listItem.append(removeBtn)
-  
     
     const quantityInCart = document.createElement('span')
     quantityInCart.classList = 'quantity-text center'
   
     const groceryTotal = cartTotal()
-    totalNumber.innerText = `£${groceryTotal}`
+    totalNumber.innerHTML = groceryTotal
   
-    const numOfItemsInCart = countItems(groceryItem)
+    const numOfItemsInCart = groceryItem.quantity
   
     quantityInCart.innerText = numOfItemsInCart
     listItem.append(quantityInCart)
   
-  
-    removeBtn.addEventListener('click', () => {subtractItemFromCart(numOfItemsInCart, groceryItem, listItem)})
-  
-    
-  
     const addBtn = document.createElement('button')
     addBtn.classList = 'quantity-btn add-btn center'
     addBtn.innerText = '+'
-    listItem.append(addBtn)
-  
-    addBtn.addEventListener('click', () => {createCartItems(groceryItem)})
+    addBtn.addEventListener('click', () => {const checkItemsInCart = cart.find((item) => item.id === groceryItem.id)
+      if (checkItemsInCart) {
+        checkItemsInCart.quantity++
+      }
+      renderCart()
+      })
     
-    listItemsInCart.innerHTML = ''
-    listItemsInCart.append(listItem)
-    cartContainer.append(listItemsInCart)
-  
+    listItem.append(addBtn)
+    console.log(cart)
+    return listItem
   }
 
-
-
-function countItems(groceryItem) {
-  
-  let totalOfEachItem = 0
-  const groceryItemId = groceryItem.id
-  const checkItemsInCart = cart.forEach((element) => {
-    if (element.id === groceryItemId && totalOfEachItem++)
-    return totalOfEachItem
-  })
-  console.log(cart, totalOfEachItem)
-  return totalOfEachItem
-}
-
-
 function cartTotal() {
-  let groceryCartTotal = 0.00
-  const checkItemPrices = cart.forEach((element) => {
-    element.price === 0.35
-    groceryCartTotal += element.price
-  })
-  return groceryCartTotal.toFixed(2)
+  const cartTotal = cart.reduce((cartTotal, groceryItem) => cartTotal += groceryItem.quantity * groceryItem.price, 0)
+  return `£${cartTotal.toFixed(2)}`
 }
 
+function subtractItemFromCart() {
 
-function subtractItemFromCart(numOfItemsInCart, groceryItem, listItem) {
-    cart.splice(1, 1)
-
-    if (numOfItemsInCart >= 2)
-      createCartItems(groceryItem)
-      else {
-        removeItemFromCart(listItem, groceryItem)
-      }
-      totalNumber.innerText = cartTotal()
+    if (groceryItem.quantity === 1) {
+      const itemIndex = cart.findIndex(item => item.id === groceryItem.id)
+      cart.splice(itemIndex, 1)
+      renderCart()
+      return
+    }
+    groceryItem.quantity--
+    renderCart()
 } 
 
-function removeItemFromCart(listItem, groceryItem) {
-  const removeListItem = listItem.remove('li')
-  cart.splice(groceryItem)
-  return cartTotal()
-}
