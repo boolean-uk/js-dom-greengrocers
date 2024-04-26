08956627d4771c0bfaa13fcfcc3ee296d76ec0a5
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-04-26 11:51:41 +0100

    not done

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-
-//This is my code!!
+// This is my code!!
 const items = state.items
-let cart = state.cart
-cart.push(items)
+const cart = state.cart
+cart.push()
 
 console.log(cart)
-
-
-
 
 function storeItems() {
     const storeItemList = document.querySelector('.store--item-list')
 
     storeItemList.innerHTML = ''
-    
+
     items.forEach(item => {
         const li = document.createElement('li')
         const imgIcon = document.createElement('img')
-       
+
         imgIcon.src = `./assets/icons/${item.id}.svg`
-        imgIcon.alt =  item.name
-        
-        //img icon buttons
-        const buttonIcon = document.createElement('button') 
+        imgIcon.alt = item.name
+
+        // img icon buttons
+        const buttonIcon = document.createElement('button')
         buttonIcon.textContent = 'add to cart'
-        
+
+        // Add cart
+        buttonIcon.addEventListener('click', () => {
+            cart.push(item)
+            myCart()
+            
+            
+        })
+
         li.append(imgIcon)
         li.append(buttonIcon)
-        
+
         storeItemList.append(li)
-       
-    })
+    });
 }
 
-storeItems() 
+storeItems()
 
-function myCart (){
-        const cartList = document.querySelector('.cart--item-list')
-                cartList.innerHTML = ''
-        
-       items.forEach(item => {
+function myCart() {
+    const cartList = document.querySelector('.cart--item-list')
+
+    cartList.innerHTML = ''
+
+    cart.forEach(item => {
         const listCart = document.createElement('li')
         const cartImage = document.createElement('img')
-        cartImage.classList.add ('cart--item-icon')
+        cartImage.classList.add('cart--item-icon')
 
         cartImage.src = `./assets/icons/${item.id}.svg`
         cartImage.alt = item.name
 
         const p = document.createElement('p')
-        p.textContent = item.name
+        p.textContent = item.name;
 
         const removeCartButton = document.createElement('button')
         removeCartButton.classList.add('remove-btn')
@@ -62,16 +66,16 @@ function myCart (){
         addCartButton.classList.add('add-btn')
         addCartButton.textContent = '+'
 
-        
-        cartList.append(listCart)
         listCart.append(cartImage)
         listCart.append(p)
         listCart.append(removeCartButton)
         listCart.append(cartSpan)
         listCart.append(addCartButton)
 
-       })
-       
+        cartList.append(listCart)
+
+
+    })
 }
-myCart()
+
 
